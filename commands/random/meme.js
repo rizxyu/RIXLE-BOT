829c9e150f67c440d606ed5192609ebe26e07d08
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-12 17:28:19 +0700

    Create meme.js

---
+++ commands/random/meme.js
@@ -0,0 +1,27 @@
+let fetch = require('node-fetch')
+
+module.exports = {
+name: ["meme"],
+type: ["random"],
+useLimit: true,
+description: "meme from redit",
+utilisation: userbot.prefix + "meme",
+
+async execute(m) {
+let { conn } = data
+try {
+    let res = await fetch('https://meme-api.herokuapp.com/gimme')
+    let json = await res.json()
+    if (json.status) throw json
+    let caption = `
+©Reddit
+Author: ${json.author} Subreddit: ${json.subreddit}
+${json.postLink}
+`.trim()
+    conn.sendFile(m.chat, json.url, 'test.jpg', caption, m)
+   } catch (e) {
+        console.log(e)
+        throw '_*Erro!*_'
+    }
+}
+}
