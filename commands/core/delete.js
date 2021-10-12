c6936aef94d84039f5d97cfe047d545b4211d68d
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-12 19:36:09 +0700

    Create delete.js

---
+++ commands/core/delete.js
@@ -0,0 +1,19 @@
+module.exports = {
+name: ["d", "delete"],
+    type: ['default'],
+    description: "delete message from bot",
+    utilisation: global.userbot.prefix+ "delete",
+async execute(m) {
+let { conn } = data
+
+if (!m.quoted) return false
+  let { chat, fromMe, id, isBaileys } = m.quoted
+  if (!fromMe) return false
+  if (!isBaileys) return m.reply('Pesan tersebut bukan dikirim oleh bot!')
+  this.deleteMessage(chat, {
+    fromMe,
+    id,
+    remoteJid: chat
+  })
+}
+}
