a920eedfaf92173b2c5251ec36341a8cf8bd14ae
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-12 11:34:50 +0700

    Create gimage.js

---
+++ commands/pencarian/gimage.js
@@ -0,0 +1,32 @@
+let { promisify } = require('util')
+let _gis = require('g-i-s')
+let gis = promisify(_gis)
+
+module.exports = {
+name: ["gimage"],
+type: ["searching"],
+useLimit: true,
+description: "google image",
+utilisation: userbot.prefix + "gimage teks",
+
+async execute(m) {
+let { conn, text } = data
+
+if (!text) return m.reply('Cari apa?')
+  let results = await gis(text) || []
+  let { url, width, height } = pickRandom(results) || {}
+  if (!url) throw '404 Not Found'
+  conn.sendFile(m.chat, url, 'gimage', `
+*── 「 GOOGLE IMAGE 」 ──*
+
+${text}
+➸ *width*: ${width}
+➸ *height*: ${height}
+`.trim(), m)
+
+}
+}
+
+function pickRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
