cdf0fd69688952fba99b7dbae9c27d87b8df7ab7
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-12 17:42:02 +0700

    Create infogempa.js
    U

---
+++ commands/media/infogempa.js
@@ -0,0 +1,20 @@
+let imageToBase64 = require('image-to-base64');
+let axios = require("axios");
+
+module.exports = {
+name: ["infogempa"],
+type: ["information"],
+useLimit: true,
+description: "informasi gempa",
+utilisation: userbot.prefix + "infogempa",
+
+async execute(m) {
+let { conn } = data
+
+await m.reply('*[❗] WAIT, Tunggu Sebentar*') 
+      axios.get(`https://arugaz.herokuapp.com/api/infogempa`).then ((res) =>{
+         let hasil = ` *Info Gempa* \n\ *Lokasi* : _${res.data.lokasi}_ \n *Kedalaman* : _${res.data.kedalaman}_ \n *Koordinat* : _${res.data.koordinat}_ \n *Magnitude* : _${res.data.magnitude}_ \n *Waktu* : _${res.data.waktu}_ `
+  
+    conn.reply(m.chat, hasil, m)
+}
+}
