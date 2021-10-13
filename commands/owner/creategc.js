3deb635494b0b10a2f6ca7377446152fe85a2ad7
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-13 19:26:42 +0700

    Create creategc.js

---
+++ commands/owner/creategc.js
@@ -0,0 +1,26 @@
+let MessageType = require('@adiwajshing/baileys');
+let chalk = require("chalk");
+
+module.exports = {
+name: ["buatgc"],
+type: ["owner"],
+description: "create grup",
+utilisation: userbot.prefix + "buatgc",
+
+async execute(m) {
+let { conn, text } = data
+
+if (!text) return m.reply('_Masukkan Nama Grup!_')
+   try{
+         await m.reply(global.wait)
+    let group = await conn.groupCreate(text, [m.sender])
+    let link = await conn.groupInviteCode(group.gid)
+    let url = 'https://chat.whatsapp.com/' + link;
+    console.log(chalk.bold.red('Membuat Grup: ' + group.gid + '\nNama Grup: ' + text + '\n\nViolet'))
+    //conn.sendMessage(group.gid, "Success to group create!", MessageType.extendedText)
+     m.reply('_Berhasil Membuat Grup *' + text + '*_\n\n*Nama:* ' + text + '\n*ID:* ' + group.gid + '\n*Link:* ' + url + '\n\n*@Violet-bot*')
+       } catch (e) {
+    m.reply('```Error```')
+    console.log (e)
+  }
+}
