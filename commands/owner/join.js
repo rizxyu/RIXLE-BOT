0987b2b146fabadb8732dee85a611ddb6e6f4d68
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-13 19:23:14 +0700

    Create join.js

---
+++ commands/owner/join.js
@@ -0,0 +1,20 @@
+let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+
+module.exports = {
+name: ["join"],
+type: ["owner"],
+description: "join grup from link",
+utilisation: userbot.prefix + "join",
+
+async execute(m) {
+let { conn, text } = data
+
+let [_, code] = text.match(linkRegex) || []
+    if (!code) return m.reply('Link invalid')
+    let res = await conn.acceptInvite(code)
+    m.reply(`Berhasil join grup ${res.gid}`)
+    for ( res.gid ) {
+     m.reply('BOT BERHASIL BERGABUNG')
+   }
+ }
+}
