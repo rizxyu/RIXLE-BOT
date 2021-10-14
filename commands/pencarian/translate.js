9843301cd34cca1863df113d8287826df6cef5b9
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-14 14:38:20 +0700

    Create translate.js

---
+++ commands/pencarian/translate.js
@@ -0,0 +1,46 @@
+const translate = require('translate-google-api')
+const defaultLang = 'en'
+const tld = 'cn'
+
+module.exports = {
+name: ["tr"],
+type: ["searching"],
+description: "mentranslate teks",
+utilisation: userbot.prefix + "tr reply",
+
+async execute(m) {
+let { conn, text, args } = data
+
+let err = `
+Contoh:
+${usedPrefix + command} <lang> [text]
+${usedPrefix + command} id your messages
+
+Daftar bahasa yang didukung: https://cloud.google.com/translate/docs/languages
+`.trim()
+
+    let lang = args[0]
+    let text = args.slice(1).join(' ')
+    if ((args[0] || '').length !== 2) {
+        lang = defaultLang
+        text = args.join(' ')
+    }
+    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+
+    let result
+    try {
+        result = await translate(`${text}`, {
+            tld,
+            to: lang,
+        })
+    } catch (e) {
+        result = await translate(`${text}`, {
+            tld,
+            to: defaultLang,
+        })
+        return m.reply(err)
+    } finally {
+        m.reply(result[0])
+    }
+}
+}
