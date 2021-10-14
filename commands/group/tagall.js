ec8c764a70c251b616b5775eec566ffb89cd045c
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-14 12:13:53 +0700

    Create tagall.js

---
+++ commands/group/tagall.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+name: ["tagall"],
+type: ["group"],
+description: "tag",
+utilisation: "#tagall",
+async execute(m) {
+let { conn, text } = data
+
+if (!text) return m.reply('Where text')
+let users = participants.map(u => u.jid)
+  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
+    contextInfo: { mentionedJid: users }
+  })
+}
+}
