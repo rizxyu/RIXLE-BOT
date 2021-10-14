ddf5b0fda76d90fc5e9a537b925e5f5bee74851a
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-14 17:12:46 +0700

    Update harrypotter.js

--- commands/maker/harrypotter.js
+++ commands/maker/harrypotter.js
@@ -8,7 +8,7 @@ name: ["harrypotter"],
 async execute(m) {
 let { conn, text } = data
 if (!text) return m.reply('teksnya mana?')
-let res = `https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=harry-potter-logo&doScale=true&scaleWidth=800&scaleHeight=400&fontsize=200&fillTextType=1&text=${text'}`
+let res = `https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=harry-potter-logo&doScale=true&scaleWidth=800&scaleHeight=400&fontsize=200&fillTextType=1&text=${text}`
 
 conn.sendFile(m.chat, await (await fetch(res)).buffer(), 'fla.jpg', '@Rizxyu', m)
 
