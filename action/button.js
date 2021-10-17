560291c033c7f204050b2d3d9c284aa81e2b775f
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 08:11:18 +0700

    Fix button

--- action/button.js
+++ action/button.js
@@ -1,18 +1,55 @@
-/*
-* MADE BY RIZKY
-*/
-const fs = Ft.fs
-const { servers, yta, ytv } = require('../Lib/y2mate')
-let { MessageType }= require('@adiwajshing/baileys')
-let { contactsArray } = MessageType
+const { MessageType }= require('@adiwajshing/baileys'),
+      { contactsArray } = MessageType,
+      fs = Ft.fs,
+      { servers, yta, ytv } = require("../Lib/y2mate.js")
 
 module.exports = {
-async execute(m, { button }) {
-let { conn, args, text } = data
+async execute(m, {button, args, text }) {
+let { conn } = data
 
 try {
-switch (button) {
-case "creator": {
+switch (button.split(" ")[0].toLowerCase()) {
+case "audio":
+  try {
+let yt = false
+let usedServer = servers[0]
+  for (let i in servers) {
+    let server = servers[i]
+    try {
+      yt = await yta(button.split(" ")[1], server)
+      usedServer = server
+      break
+    } catch (e) {
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+    }
+  }
+if (yt === false) throw 'semua server gagal'
+conn.sendFile(m.chat,yt.dl_link,"m.mp3",null,m)
+} catch (e) {
+throw e
+}
+break;
+case "video":
+  try {
+let yt = false
+let usedServer = servers[0]
+  for (let i in servers) {
+    let server = servers[i]
+    try {
+      yt = await ytv(button.split(" ")[1], server)
+      usedServer = server
+      break
+    } catch (e) {
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+    }
+  }
+if (yt === false) throw 'semua server gagal'
+conn.sendFile(m.chat,yt.dl_link,"m.mp3",null,m)
+} catch (e) {
+throw e
+}
+break;
+case "creator":
 conarray = []
 ownerContact = ['6282328303332', '62822980698995','6285783417029','62823283033323','6285640020165','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0']
   for (let i of ownerContact.map(v => v + '@s.whatsapp.net')) {
@@ -27,33 +64,10 @@ conn.sendMessage(m.chat, {
 "contacts": conarray 
 }, 'contactsArrayMessage', { quoted: m })
 conn.sendMessage(m.chat, 'Ini nomer pembuat bot gak usah chat aneh aneh ya!',MessageType.text, { quoted: m} )
-}
-break
-case "video": {
-m.reply('nothing')
-}
-break
-case "audio": {
-if (!args[0]) return m.reply('linknya mana')
-let server = (args[1] || servers[0]).toLowerCase()
-let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
-
-conn.sendFile(m.chat, dl_link, title + '.mp3', null, m)
-}
-break
-case "rules": {
-teks = `👑ATURAN MEMAKAI BOT:
-DILARANG SPAM COMMAND
-JANGAN BANDINGIN BOT LAIN
-
-EN: RULES FOR USING BOTS:
-NO SPAM COMMAND
-DON'T COMPARE OTHER BOTS
-
-BY FEAR TEAM`
-m.reply(teks)
-}
-break
+console.log(a)
+break;
+default:
+throw new Error(`command ${button} tidak ada :v`) //biar tau soalny kesusahan di buat button audio asw
 }
 } catch (e) {
 console.log(e)
