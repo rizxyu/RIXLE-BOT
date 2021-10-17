e3cb12c0b11d569cb46f0122378af7b5a56e9d14
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2021-10-17 18:36:12 +0700

    Maap lupa

--- Lib/converter.js
+++ Lib/converter.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const fetch = require("node-fetch")
 const { spawn } = require('child_process')
 
 function ffmpeg(buffer, args = [], ext = '', ext2 = '') {
