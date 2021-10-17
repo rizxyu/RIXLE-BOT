4fdc3822421882bcd1d308df5fbc3c49848f5e2a
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 11:52:45 +0700

    Update converter.js

--- Lib/converter.js
+++ Lib/converter.js
@@ -2,6 +2,35 @@ const fs = require('fs')
 const path = require('path')
 const { spawn } = require('child_process')
 
+function ffmpeg(buffer, args = [], ext = '', ext2 = '') {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let tmp = path.join(__dirname, '../tmp', + new Date + '.' + ext)
+      let out = tmp + '.' + ext2
+      await fs.promises.writeFile(tmp, buffer)
+      spawn('ffmpeg', [
+        '-y',
+        '-i', tmp,
+        ...args,
+        out
+      ])
+        .on('error', reject)
+        .on('close', async (code) => {
+          try {
+            await fs.promises.unlink(tmp)
+            if (code !== 0) return reject(code)
+            resolve(await fs.promises.readFile(out))
+            await fs.promises.unlink(out)
+          } catch (e) {
+            reject(e)
+          }
+        })
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
 function toAudio(buffer, ext) {
   return new Promise((resolve, reject) => {
     let tmp = path.join(__dirname, './', + new Date  + '.' + ext)
@@ -79,5 +108,6 @@ function toVideo(buffer, ext) {
 module.exports = {
   toAudio,
   toPTT,
-  toVideo
+  toVideo,
+  ffmpeg
 }
