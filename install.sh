465e90a74f0d77bad058d6294569c5c8e6b548c9
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-10-17 15:47:40 +0700

    Create install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
-pkg install nodejs -y
-pkg install ffmpeg -y
-pkg install imagemagick -y
-npm install
-npm update
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+reset='\033[0m'
+
+clear
+apt install ffmpeg imagemagick nodejs neofetch 
+npm i
+npm start
+printf "$yellow Startring..."
