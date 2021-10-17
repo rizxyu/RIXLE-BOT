cb16ddf961aae86a74724febf67b246de110cd4c
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 12:03:59 +0700

    Update sticker2.js

--- commands/build/sticker2.js
+++ commands/build/sticker2.js
@@ -59,7 +59,8 @@ let stiker = false
       quoted: m
     })
   }
-
+}
+}
 
 const isUrl = (text) => {
   return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
