a5ec14a023c8f1d65e5949f072846edb0a915ea2
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 13:47:19 +0700

    Update github.js

--- commands/pencarian/github.js
+++ commands/pencarian/github.js
@@ -1,4 +1,4 @@
-let { ghstalk } = require("../../Lib/scrape")
+let scrap = require("../../Lib/scrape")
 module.exports = {
 name: ["ghstalk"],
 type: ["searching"],
@@ -9,7 +9,7 @@ async execute(m){
 let { conn, text } = data
 if (!text) return m.reply("Username?")
 
-const { login, id, avatar_url, url, html_url, followers_url, following_url, subscriptions_url, repos_url, type, site_admin, location, email, bio, public_repos, public_gists, following, followers, created_at, updated_at } = await ghstalk.ghstalk(text)
+const { login, id, avatar_url, url, html_url, followers_url, following_url, subscriptions_url, repos_url, type, site_admin, location, email, bio, public_repos, public_gists, following, followers, created_at, updated_at } = await scrap.ghstalk(text)
 conn.sendFile(m.chat, avatar_url, null,`*GITHUB STALK*\n
 username: ${login}
 id: ${id}
