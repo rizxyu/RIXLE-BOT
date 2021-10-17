f438b642cf69780bee67387ec934710069e6d882
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-18 05:36:56 +0700

    Update Smeme.js

--- commands/maker/Smeme.js
+++ commands/maker/Smeme.js
@@ -1,6 +1,6 @@
-const uploadImage = require('../lib/uploadImage')
+const uploadImage = require('../../Lib/uploadImage')
 const { MessageType } = require('@adiwajshing/baileys')
-const { sticker } = require('../lib/sticker')
+const { sticker } = require('../../Lib/sticker')
 
 module.exports = {
  name: ['smeme'],
@@ -14,7 +14,7 @@ let [atas, bawah] = text.split`|`
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (!mime) return m.reply(`balas gambar dengan perintah\n\n${userbot.prefix + command} <${atas ? atas : 'teks atas'}>|<${bawah ? bawah : 'teks bawah'}>`)
-    if (!/image\/(jpe?g|png)/.test(mime)) throw `_*Mime ${mime} tidak didukung!*_`
+    if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`_*Mime ${mime} tidak didukung!*_`)
     let img = await q.download()
     let url = await uploadImage(img)
     meme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas ? atas : '_')}/${encodeURIComponent(bawah ? bawah : '_')}.png?background=${url}`
