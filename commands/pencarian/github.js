81bf7ee24794ff4a2d6d3279b9bb8c9a4adc165c
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2021-10-17 12:42:23 +0700

    Create github.js

---
+++ commands/pencarian/github.js
@@ -0,0 +1,33 @@
+module.exports = {
+name: ["ghstalk"],
+type: ["searching"],
+description: "search username from github.com",
+utilisation: userbot.prefix+"ghstalk",
+
+async execute(m){
+var usr = m.text.slice(9)
+if (!usr) return m.reply("Username?")
+try {
+var { login, id, avatar_url, url, html_url, followers_url, following_url, subscriptions_url, repos_url, type, site_admin, location, email, bio, public_repos, public_gists, following, followers, created_at, updated_at } = await scrap.ghstalk(usr)
+conn.sendFile(m.chat, avatar_url, null,`*GITHUB STALK*\n
+username: ${login}
+id: ${id}
+url: ${html_url}
+user_url: ${url}
+type: ${type}
+admin: ${site_admin ? "ya" : "tidak"}
+location: ${location}
+email: ${email}
+repo: ${public_repos}
+gists: ${public_gists}
+followers: ${followers}
+following: ${following}
+create: ${created_at}
+update: ${updated_at}
+`,m) //add sendiri cape gw ajg
+} catch (e) {
+console.log(e)
+m.reply("Username tidak ditemukan")
+}
+}
+}
