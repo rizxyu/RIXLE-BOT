8866d4701240c157bf7da8d8ea9e17ef1e7414b1
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 07:54:53 +0700

    Create jadibot_gcode.js

---
+++ commands/build/jadibot_gcode.js
@@ -0,0 +1,11 @@
+module.exports = {
+name: ["getcode"],
+type: ["session"],
+description: "get code jadibot",
+utilisation: "#getcode",
+async execute(m) {
+let { conn } = data
+if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Perintah ini hanya untuk yang jadi bot', m)
+  else global.conn.reply(conn.user.jid, `${usedPrefix}jadibot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
+}
+}
