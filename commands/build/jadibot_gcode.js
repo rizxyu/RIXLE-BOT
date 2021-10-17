feedf0575e884ec50d44806a45ae4804720d6097
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 07:56:04 +0700

    Update jadibot_gcode.js

--- commands/build/jadibot_gcode.js
+++ commands/build/jadibot_gcode.js
@@ -6,6 +6,6 @@ utilisation: "#getcode",
 async execute(m) {
 let { conn } = data
 if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'Perintah ini hanya untuk yang jadi bot', m)
-  else global.conn.reply(conn.user.jid, `${usedPrefix}jadibot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
+  else global.conn.reply(conn.user.jid, `${userbot.prefix}jadibot ${Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString('base64')}`, m)
 }
 }
