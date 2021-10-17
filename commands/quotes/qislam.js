907d0095404a0bc733c36b98141999e3eb42c454
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 21:17:33 +0700

    Update qislam.js

--- commands/quotes/qislam.js
+++ commands/quotes/qislam.js
@@ -12,22 +12,6 @@ utilisation: userbot.prefix + "qislam",
 
 async execute(m) {
  let { conn, text } = data
-
-let cuk = await kntl('const kntl = require("node-fetch")
-const fs = Ft.fs
-
-module.exports = {
-
-name: ["heker"],
-
-type: ["quotes"],
-useLimit: true,
-description: "quotes kata kata heker tzy",
-utilisation: userbot.prefix + "heker",
-
-async execute(m) {
- let { conn, text } = data
-
 let cuk = await fetch('https://raw.githubusercontent.com/Rizxyu/FEATURE-BOT/main/random/QuotesIslami.json')
 let dot = await cuk.json()
     let json = dot[Math.floor(Math.random() * dot.length)]
