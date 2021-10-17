c2085b27d3c4529bc7215425a4fd3f495065ca8b
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 10:31:09 +0700

    Update jadibot_list.js

--- commands/build/jadibot_list.js
+++ commands/build/jadibot_list.js
@@ -6,6 +6,6 @@ utilisation: "#listjadibot",
 async execute(m) {
 let { conn } = data
 let users = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
-  m.reply(users.map(v => 'wa.me/' + v.jid.replace(/[^0-9]/g, 'TIDAK ADA YG JADI BOT\n\n*BOT BY RIZKY ADI*') + `?text=${userbot.prefix}menu (${v.name})`).join('\n'))
+  m.reply(users.map(v => 'wa.me/' + v.jid.replace(/[^0-9]/g, '') + `?text=${userbot.prefix}menu (${v.name})`).join('\n'))
 }
 }
