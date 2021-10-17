f431ad581910d8e29e0507bb9994668934b12bdd
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 13:08:23 +0700

    Create Kodepos.js

---
+++ commands/pencarian/Kodepos.js
@@ -0,0 +1,20 @@
+let scrap = require("../../Lib/scrape")
+
+module.exports = {
+name: ["kodepos"],
+type: ["searching"],
+description: "search username from github.com",
+utilisation: userbot.prefix+"ghstalk",
+
+async execute(m){
+let { conn, text } = data
+if (!text) return m.reply('lokasi')
+
+var s = await scrap.kodepos(text)
+let capt = "*KODEPOS SEARCH*\n\n"
+for (let i = 0; i < s.length; i++) {
+capt +=`*Provinsi:* ${s[i].province}\n*Kota:* ${s[i].city}\n*kecamatan:* ${s[i].subdistrict}\n*perkotaan:* ${s[i].urban}\n*kode:* ${s[i].postalcode}\n\n`
+}
+m.reply(capt)
+}
+}
