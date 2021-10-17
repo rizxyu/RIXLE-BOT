d4ac1605cddec0a3912f08033987e9c1ac2d509b
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 18:16:12 +0700

    Create toimg.js

---
+++ commands/maker/toimg.js
@@ -0,0 +1,25 @@
+let { webp2png } = require('../lib/webp2mp4')
+
+module.exports = {
+
+name: ["toimg"],
+
+type: ["maker"],
+useLimit: true,
+description: "sticker to image",
+utilisation: userbot.prefix + "toimg (reply)",
+
+async execute(m) {
+ let { conn } = data
+
+if (!m.quoted) throw `balas stiker dengan perintah *${userbot.prefix + command}*`
+  let mime = m.quoted.mimetype || ''
+  if (!/webp/.test(mime)) throw `balas stiker dengan perintah *${userbot.prefix + command}*`
+  let media = await m.quoted.download()
+  let out = Buffer.alloc(0)
+  if (/webp/.test(mime)) {
+    out = await webp2png(media)
+  }
+  await conn.sendFile(m.chat, out, 'out.png', '© stikerin', m, false, { thumbnail: out })
+}
+}
