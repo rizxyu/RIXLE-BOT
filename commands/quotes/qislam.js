850a8f2a910c9c584c87b98a6ed960bd4f887cd7
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 21:30:09 +0700

    Update qislam.js

--- commands/quotes/qislam.js
+++ commands/quotes/qislam.js
@@ -4,18 +4,16 @@ const fs = Ft.fs
 module.exports = {
 
 name: ["qislam"],
-
 type: ["quotes"],
 useLimit: true,
 description: "quotes kata kata islami",
 utilisation: userbot.prefix + "qislam",
 
 async execute(m) {
- let { conn, text } = data
+ let { conn } = data
 let cuk = await fetch('https://raw.githubusercontent.com/Rizxyu/FEATURE-BOT/main/random/QuotesIslami.json')
 let dot = await cuk.json()
-    let json = dot[Math.floor(Math.random() * dot.length)]
-
+let json = dot[Math.floor(Math.random() * dot.length)]
 m.reply(json)
 }
 }
