fe5125f35d4c5c05d509b1826f6a76f3505865eb
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-10-17 15:35:41 +0700

    Add files via upload

--- commands/maker/ttp.js
+++ commands/maker/ttp.js
@@ -1,18 +1,14 @@
-let { exec } = require('child_process')
 module.exports = {
- name: ['attp'],
- type: ['maker'],
- description: null,
- utilsation: userbot.prefix + "attp",
+ name: ['ttp'],
+ type: ['Sticker'],
+ description: "untuk membuat sticker teks bergerak",
+ utilsation: null,
  
 async execute(m) {
  let { text, conn } = data
  if (!text) return m.reply("teks??")
  let media = await Ft.getBuffer(`https://api.xteam.xyz/attp?file&text=${encodeURI(text)}`)
-conn.sendMessage(m.chat, media, 'stickerMessage', {})
-}
-}
 
-function getRandom (ext) {
-return `${Math.floor(Math.random() * 10000)}${ext}`
+conn.sendMessage(m.chat, media, 'stickerMessage', {quoted: m})
+}
 }
---
+++ commands/maker/ttp2.js
@@ -0,0 +1,28 @@
+let fs = require("fs")
+module.exports = {
+ name: ['ttp'],
+ type: ['Sticker'],
+ description: "untuk membuat sticker teks bergerak",
+ utilsation: null,
+ 
+async execute(m) {
+ let { text, conn } = data
+ if (!text) return m.reply("teks??")
+ran = getRandom(".webp")
+int = getRandom(".jpg")
+ let p = await Ft.getBuffer(`https://api.xteam.xyz/ttp?file&text=${text}`)
+Ft.fs.writeFileSync(`${int}`, p)
+let ipt = await Ft.fs.readFileSync(`${int}`)
+Ft.exec(`convert ${int} ${ran}`)
+.on("error", () => reply("error"))
+await sleep()
+conn.sendMessage(m.chat, Ft.fs.readFileSync(`${ran}`), "stickerMessage")
+
+function getRandom(ext) {
+	return `${Math.floor(Math.random() * 10000)}${ext}`
+}
+}
+}
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms*1000));
+}
