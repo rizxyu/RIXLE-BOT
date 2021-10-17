f39dde563f222571c4d4bdb74909415b2ddf3f7e
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 13:16:36 +0700

    Update github.js

--- commands/pencarian/github.js
+++ commands/pencarian/github.js
@@ -7,10 +7,9 @@ utilisation: userbot.prefix+"ghstalk",
 
 async execute(m){
 let { conn, text } = data
-var usr = m.text.slice(9)
 if (!text) return m.reply("Username?")
 try {
-var { login, id, avatar_url, url, html_url, followers_url, following_url, subscriptions_url, repos_url, type, site_admin, location, email, bio, public_repos, public_gists, following, followers, created_at, updated_at } = await ghstalk.ghstalk(usr)
+const { login, id, avatar_url, url, html_url, followers_url, following_url, subscriptions_url, repos_url, type, site_admin, location, email, bio, public_repos, public_gists, following, followers, created_at, updated_at } = await ghstalk.ghstalk(text)
 conn.sendFile(m.chat, avatar_url, null,`*GITHUB STALK*\n
 username: ${login}
 id: ${id}
@@ -28,8 +27,7 @@ create: ${created_at}
 update: ${updated_at}
 `,m) //add sendiri cape gw ajg
 } catch (e) {
-console.log(e)
-m.reply("Username tidak ditemukan")
+ m.reply(e)
 }
 }
 }
