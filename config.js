02801cd0cfee842a633d0edba59aadfee5d8149f
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 21:13:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 userbot = {
- owner: [ "62823283033323", "62822980698995", "436504463151666", "6282328303332" ],
+ owner: [ "6282328303332", "62822980698995", "436504463151666", "62823283033323", "628812904283" ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
