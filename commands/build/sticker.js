3a8209861c24f68f4535956b3006f3736a79db28
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-17 12:23:16 +0700

    Fix eror

--- commands/build/sticker.js
+++ commands/build/sticker.js
@@ -1,23 +1,68 @@
-const { exec } = require('child_process')
+const { MessageType } = require('@adiwajshing/baileys')
+const { sticker } = require('../../Lib/sticker')
+const WSF = require('wa-sticker-formatter')
 
 module.exports = {
 name: ["sticker"],
 type: ["create"],
-description: "membuat command",
-utilisation: "#sticker <reply sticker>",
+description: "create sticker with waf",
+utilisation: "#sticker (reply)",
 async execute(m) {
-let encmedia = JSON.parse(JSON.stringify(m).replace('quotedM','m')).message.extendedTextMessage.contextInfo
-let media = await conn.downloadAndSaveMediaMessage(encmedia)
-let ran = getRandom('.webp')
-exec(`convert ${media} ${ran}`, (err) => {
-if (err) return m.reply("error")
-conn.sendMessage(m.chat, Ft.fs.readFileSync(ran), mediaType.sticker, { quoted: m })
-Ft.fs.unlinkSync(ran) 
-Ft.fs.unlinkSync(media)
-});
+let { conn } = data
+
+let stiker = false
+  let wsf = false
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (/webp/.test(mime)) {
+      let img = await q.download()
+      if (!img) return m.reply(`balas stiker dengan perintah ${userbot.prefix}s`)
+      wsf = new WSF.Sticker(img, {
+        pack: userbot.packname,
+        author: userbot.author,
+        crop: false,
+      })
+    } else if (/image/.test(mime)) {
+      let img = await q.download()
+      if (!img) return m.reply(`balas image dengan perintah ${userbot.prefix}s`)
+      wsf = new WSF.Sticker(img, {
+        pack: userbot.packname,
+        author: userbot.author,
+        crop: false,
+      })
+    } else if (/video/.test(mime)) {
+      if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+      let img = await q.download()
+      if (!img) return m.reply(`balas video dengan perintah ${userbot.prefix}s`)
+       wsf = new WSF.Sticker(img, {
+        pack: userbot.packname,
+        author: userbot.author,
+        crop: true,
+      })
+    } else if (args[0]) {
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], userbot.packname, userbot.author)
+      else return m.reply('URL tidak valid!')
+    }
+  } catch (e) {
+    throw e
+  }
+  finally {
+    if (wsf) {
+      await wsf.build()
+      const sticBuffer = await wsf.get()
+      if (sticBuffer) await conn.sendMessage(m.chat, sticBuffer, MessageType.sticker, {
+        quoted: m,
+        mimetype: 'image/webp'
+      })
+    }
+    if (stiker) await conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+      quoted: m
+    })
+  }
 }
 }
 
-function getRandom (ext) {
-return `${Math.floor(Math.random() * 10000)}${ext}`
+const isUrl = (text) => {
+  return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png)/, 'gi'))
 }
