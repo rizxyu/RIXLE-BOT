122c8bea0b59829c43f2047fa983a38e82369661
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-18 14:34:37 +0700

    Update action.js

--- action/action.js
+++ action/action.js
@@ -1,4 +1,5 @@
-let { MessageType } = require("@adiwajshing/baileys")
+let { MessageType, mentionedJid } = require("@adiwajshing/baileys")
+let fetch = Ft.fetch
 module.exports = {
 async battery(json) {
 let battery = json[2][0][1].value
@@ -18,18 +19,21 @@ console.log(member)
 let groupM = await conn.groupMetadata(member.jid)
 let mem = member.participants[0]
 let action = member.action
+try {
+let img = Ft.getBuffer(await vanz.getProfilePicture(mem))
+} catch {
+img = await (await fetch('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa1pVfdb1zUoSve4Unc08jl5BpCHwfys8qxA&usqp=CAU')).buffer()
+}
 switch (action) {
-case 'add': {
-teks = `member @${mem.split("@")[0]} bergabung ke group ${groupM.subject}`
-conn.sendMessage(member.jid, teks, MessageType.text, {
-contextInfo: { mentionedJid: teks}})
-}
+case 'remove': 
+teks = `@${mem.split("@")[0]} Keluar Dari Group ${groupM.subject}`
+conn.sendMessage(member.jid, teks, MessageType.text, {thumbnail: img,
+contextInfo: {"mentionedJid": conn.parseMention(teks)}})
 break
-case 'remove': {
-teks = `member yang keluar @${mem.split("@")[0]} dari group ${groupM.subject}`
-conn.sendMessage(member.jid, teks, MessageType.text, {
-contextInfo: { mentionedJid: teks}})
-}
+case 'add' : 
+teks = `@${mem.split("@")[0]} Bergabung ke Dalam Group ${groupM.subject}`
+conn.sendMessage(member.jid, teks, MessageType.text, {thumbnail: img,
+contextInfo: {"mentionedJid": conn.parseMention(teks)}})
 break
 }
 }
