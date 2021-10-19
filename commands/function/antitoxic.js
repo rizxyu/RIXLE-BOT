b18681831c36e7d7626fafc692d53cd5fa8baa71
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-19 12:47:49 +0700

    Update antitoxic.js

--- commands/function/antitoxic.js
+++ commands/function/antitoxic.js
@@ -2,11 +2,11 @@ module.exports = {
 name: "antitoxic",  
 async functions(m) { 
 let { conn, text } = data
-let badwordRegex = ['anjing']
+let badwordRegex = /anjing/i
 
 let name = conn.getName(m.sender)
 if (m.isBaileys && m.fromMe) return  !0
-let isBadword = badwordRegex(m.text)
+let isBadword = badwordRegex.exec(m.text)
 
 if (!isBadword) {
 m.reply( name + 'Terdeteksi Menggunakan bahasa kotor, Kami hanya memberi mu warn\n@0')
