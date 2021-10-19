e34a9795addd63838be1671306e0dde0d3001f40
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-19 10:27:46 +0700

    Create pat.js

---
+++ commands/maker/pat.js
@@ -0,0 +1,35 @@
+const { MessageType } = require("@adiwajshing/baileys");
+const petPetGif = require("pet-pet-gif");
+const { createSticker, StickerTypes } = require("wa-sticker-formatter");
+
+module.exports = {
+ name: ['pat'],
+ type: ['sticker'],
+ description: "untuk membuat sticker image Pat pat",
+ utilsation: userbot.prefix + "pat",
+ 
+async execute(m) {
+ let { text, conn } = data
+
+  let who =
+    m.mentionedJid && m.mentionedJid[0]
+      ? m.mentionedJid[0]
+      : m.fromMe
+      ? conn.user.jid
+      : m.sender;
+  let animatedGif = await petPetGif(
+    await conn
+      .getProfilePicture(who)
+      .catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+  );
+  const sticker = await createSticker(animatedGif, {
+    type: StickerTypes.FULL,
+    pack: userbot.packname,
+    author: userbot.author,
+  });
+  await conn.sendMessage(m.chat, sticker, MessageType.sticker, {
+    quoted: m,
+    mimetype: "image/webp",
+  });
+};
+};
