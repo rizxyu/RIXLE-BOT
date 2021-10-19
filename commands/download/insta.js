67fa0ae89a90849b9f1c9a969da187d71687757f
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2021-10-19 19:49:02 +0700

    Create insta.js

---
+++ commands/download/insta.js
@@ -0,0 +1,21 @@
+module.exports = {
+ name: ['ig'],
+ type: ['download'],
+ description: "untuk download ig",
+ utilsation: '#ig link type',
+ 
+async execute(m) {
+let { args, conn } = data
+let { igvid, igfoto } = require("../../Lib/scrape")
+if (args[1] === 'foto') {
+await igfoto(args[0]).then(res => {
+ conn.sendFile(m.chat, res.link, null, null, m)
+})
+}
+if (args[1] === 'video') {
+await igvideo(args[0]).then(res => {
+ conn.sendFile(m.chat, res.link, null, null, m)
+})
+}
+}
+}
