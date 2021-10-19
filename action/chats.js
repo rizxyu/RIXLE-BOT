801f5f4b6189bf70297e62e76c68174b9d9d328d
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-19 18:34:38 +0700

    Hm

--- action/chats.js
+++ action/chats.js
@@ -13,7 +13,7 @@ require('../Lib/simple')['smsg'](this, m);
 let body = (mess === 'conversation' && m.text.startsWith(prefix)) ? m.text : (mess == 'imageMessage') && m.message.imageMessage.caption.startsWith(prefix) ? m.message.imageMessage.caption : (mess == 'videoMessage') && m.message.videoMessage.caption.startsWith(prefix) ? m.message.videoMessage.caption : (mess == 'extendedTextMessage') && m.message.extendedTextMessage.text.startsWith(prefix) ? m.message.extendedTextMessage.text : ""
 let message = (mess === 'conversation') ? m.message.conversation : (mess === 'extendedTextMessage') ? m.message.extendedTextMessage.text : ''
 if (message.startsWith(prefix)) console.log(Ft['color']('\x1b[1;37m>', 'cyan'), Ft['color']('[CMD]'), Ft['color'](Ft['moment'].tz('Asia/Jakarta').format('DD/MM/YYYY HH:mm'), 'cyan'), Ft['color']('Command', 'cyan'), 'Dari', Ft['color'](await this.getName(m.sender)));
-else console.log(Ft['color']('\x1b[1;37m>', 'cyan'), Ft['color']('[MSG]'), Ft['color'](Ft['moment'].tz('Asia/Jakarta').format('DD/MM/YYYY HH:mm'), 'cyan'), Ft['color']('Message', 'yellow'), 'Dari', Ft['color'](await this.getName(m.sender)));
+else console.log(Ft['color']('\x1b[1;37m>', 'cyan'), Ft['color']('[Message]'), Ft['color'](Ft['moment'].tz('Asia/Jakarta').format('DD/MM/YYYY HH:mm'), 'cyan'), Ft['color']('Message', 'yellow'), 'Dari', Ft['color'](await this.getName(m.sender)));
 global.command = body.replace(prefix, '').trim().split(/ +/).shift().toLowerCase();
 require('./cmd.js').Command(this, m);
 }
