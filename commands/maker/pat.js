0a5a1803105f3238bef54b3438797abad2a61542
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-19 10:30:06 +0700

    Update pat.js

--- commands/maker/pat.js
+++ commands/maker/pat.js
@@ -1,6 +1,6 @@
-const { MessageType } = require("@adiwajshing/baileys");
-const petPetGif = require("pet-pet-gif");
-const { createSticker, StickerTypes } = require("wa-sticker-formatter");
+const { MessageType } = require("@adiwajshing/baileys")
+const petPetGif = require("pet-pet-gif")
+const { createSticker, StickerTypes } = require("wa-sticker-formatter")
 
 module.exports = {
  name: ['pat'],
@@ -21,15 +21,15 @@ async execute(m) {
     await conn
       .getProfilePicture(who)
       .catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
-  );
+  )
   const sticker = await createSticker(animatedGif, {
     type: StickerTypes.FULL,
     pack: userbot.packname,
     author: userbot.author,
-  });
+  })
   await conn.sendMessage(m.chat, sticker, MessageType.sticker, {
     quoted: m,
     mimetype: "image/webp",
-  });
-};
-};
+  })
+}
+}
