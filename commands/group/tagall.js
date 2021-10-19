93af9524dc3e8ab1bde19e01f5dcace540a39845
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-20 06:55:43 +0700

    Update tagall.js

--- commands/group/tagall.js
+++ commands/group/tagall.js
@@ -3,12 +3,13 @@ module.exports = {
 name: ["tagall"],
 type: ["group"],
 description: "tag",
+admin: true,
 utilisation: "#tagall",
-async execute(m) {
-let { conn, text, participants } = data
+async execute(m, member) {
+let { conn, text } = data
 
 if (!text) return m.reply('Where text')
-let users = participants.map(u => u.jid)
+let users = member.participants.map(u => u.jid)
   m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
     contextInfo: { mentionedJid: users }
   })
