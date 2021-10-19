e268741fbae99ea0ac8242a280733171ea78063f
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2021-10-19 19:51:01 +0700

    Lupa :v

--- commands/download/insta.js
+++ commands/download/insta.js
@@ -7,6 +7,7 @@ module.exports = {
 async execute(m) {
 let { args, conn } = data
 let { igvid, igfoto } = require("../../Lib/scrape")
+if(!args[0]) return m.reply("Link?")
 if (args[1] === 'foto') {
 await igfoto(args[0]).then(res => {
  conn.sendFile(m.chat, res.link, null, null, m)
