a25343a5b1e5eee78f203761099ea95260f242cb
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-19 12:34:38 +0700

    Update antitoxic.js

--- commands/function/antitoxic.js
+++ commands/function/antitoxic.js
@@ -5,12 +5,12 @@ let { conn, text } = data
 let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i 
 
 let name = conn.getName(m.sender)
-if (m.isBaileys && m.fromMe) return  false
+//if (m.isBaileys && m.fromMe) return  false
 let isBadword = badwordRegex.exec(m.text)
 
 if (!isBadword) {
 m.reply( name + 'Terdeteksi Menggunakan bahasa kotor, Kami hanya memberi mu warn\n@0')
 }
-return false
+//return false
 }
 }
