ebcfb4c11f3da8885f90bf2fd15655f9748e0c62
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-20 15:46:35 +0700

    Fix fetch

--- commands/core/fetch.js
+++ commands/core/fetch.js
@@ -13,13 +13,13 @@ let { conn, text } = data
 
 if (!/^https?:\/\//.test(text)) return m.reply('Awali *URL* dengan http:// atau https://')
   let _url = new URL(text)
-  let url = global.API(_url.origin, _url.pathname, Object.fromEntries(_url.searchParams.entries()), 'APIKEY')
+  let url = `${_url}`
   let res = await fetch(url)
   if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
     delete res
-    throw `Content-Length: ${res.headers.get('content-length')}`
+    return m.reply(`Content-Length: ${res.headers.get('content-length')}`)
   }
-  if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, 'file', text, m)
+  if (!/text|json/.test(res.headers.get('content-type'))) return conn.sendFile(m.chat, url, null, text, m)
   let txt = await res.buffer()
   try {
     txt = util.format(JSON.parse(txt+''))
