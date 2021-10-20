72ef1ef30a0e1f7cba40ab9a0b01dca5e437cba4
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2021-10-20 11:15:04 +0700

    Create sepia.js

---
+++ commands/maker/sepia.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: ["sepia"],
+  type: ["image"],
+  description: "make sepia images",
+  utilisation: userbot.prefix+"sepia",
+  
+  async execute(m){
+    var input = "a.png"
+    var output = "b.png"
+    Ft.fs.writeFileSync(input,await m.quoted.download())
+    var jInpt = Ft.Jimp.read(input)
+
+Promise.all([jInpt]).then(function(images){
+  const a = images[0];
+  a
+  .sepia()
+  .quality(60)
+  .background(0)
+  .resize(256, Ft.Jimp.AUTO)
+  .write(output, (err)=>{
+    if(err){
+     	console.log(err)
+    } else {
+      conn.sendFile(m.chat,Ft.fs.readFileSync(output), output,"y")
+    }
+  })
+}).catch((e) => console.log(e) && client.reply("Error!"))
+  }
+}
