70b8c37cbffec01c9a28e50ad0f431bef8641d2b
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-21 06:40:30 +0700

    Update reverb.js

--- commands/audio-convert/reverb.js
+++ commands/audio-convert/reverb.js
@@ -1,57 +1,35 @@
-const util = Ft.util
-const path = Ft.path
-const fs = Ft.fs
-const ffmpeg = Ft.ffmpeg
-const sox = require("sox-audio")
+const fs = require('fs')
+const { exec } = require('child_process')
 
 module.exports = {
 name: ["reverb"],
 type: ["audio"],
-description: "reverb effects audio",
-utilisation: "#reverb <reply audio>",
+useLimit: true,
+description: "convert audio music to reverb",
+utilisation: userbot.prefix + "reverb",
+
 async execute(m) {
-let {conn} = data
-const dir = path.join(__dirname, "..", "/tmp/", getRandom(".mp3"))
-if (Object.keys(m.message)[0] != 'extendedTextMessage') return reply("Reply audio nya!")
-const encmed = JSON.parse(JSON.stringify(m.message.extendedTextMessage.contextInfo).replace("quotedM", "m"))
-if (Object.keys(encmed.message)[0] != mediaType.audio) return reply("Media tersebut harus audio")
-const rdir = path.join(__dirname, "..", "/tmp/", getRandom(".mp3"))
-const audio = await conn.downloadMediaMessage(encmed, "stream")
-ffmpeg(audio) 
-.audioQuality(1)
-.audioFrequency(48000)
-.save(rdir)
-.on('start', function (commandLine) {
-console.log('Spawned Ffmpeg with command: ' + commandLine);
-})
-.on('error', function (err, stdout, stderr) {
- return new Error('An error occurred: ' + err.message, err, stderr);
-})
-.on('end', async () => {
-sox(rdir)
-.inputBits(192)
-.inputSampleRate('48k')
-.addEffect('gain', ['-3'])
-.addEffect('pad', ['0', '6'])
-.addEffect('reverb', ['100', '25', '75', '100', '0', '3'])
-.output(dir)
-.outputSampleRate('48k')
-.outputBits(192)
-.on('end', async () => {
-audio = await fs.readFileSync(dir)
-await conn.sendMessage(m.chat, audio, mediaType.audio, {
-quoted: m,
-mimetype: 'audio/mpeg',
-mp3: true
-})
-await fs.unlinkSync(rdir)
-await fs.unlinkSync(dir)
-})
-.run();
-})
+let { conn } = data
+try {
+        let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
+        let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
+        if (/audio/.test(mime)) {
+            let media = await conn.downloadAndSaveMediaMessage(q)
+            let ran = getRandom('.mp3')
+            exec(`ffmpeg.exe -i ${media} -map 0 -c:v copy -af aecho=1.0:0.7:20:0.5 ${ran}`, (err, stderr, stdout) => {
+                fs.unlinkSync(media)
+                if (err) m.reply(`_*Error!*_`)
+                let buff = fs.readFileSync(ran)
+                conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
+                fs.unlinkSync(ran)
+            })
+        } else m.reply(`Balas vn/audio yang ingin diubah dengan caption *${userbot.prefix}reverb*`)
+    } catch (e) {
+        throw e
+    }
 }
 }
 
-function getRandom (ext) {
-return `${Math.floor(Math.random() * 10000)}${ext}`
+const getRandom = (ext) => {
+    return `${Math.floor(Math.random() * 10000)}${ext}`
 }
