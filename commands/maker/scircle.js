ac83a58d6631cf90a0805cb81ce73d86d4d6e7d3
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-20 16:50:04 +0700

    Update scircle.js

--- commands/maker/scircle.js
+++ commands/maker/scircle.js
@@ -16,9 +16,7 @@ let qe = m.quoted ? m.quoted : m
   let mime = (qe.msg || qe).mimetype || ""
   if (/image/.test(mime)) {
   let img = await qe.download();
-  Canvas.circle(img).then((p) =>
-  stiker = await sticker(false, p, userbot.packname, userbot.author))
-     conn.sendMessage(m.chat, stiker, 'stickerMessage', {quoted: m})
+  Canvas.circle(img).then((p) => conn.sendMessage(m.chat, stiker, 'stickerMessage', {quoted: m}))
   } else return m.reply('reply image')
 }
 }
