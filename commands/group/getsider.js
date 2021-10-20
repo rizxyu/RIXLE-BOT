63f21fd8aedd9eb29b4a51d1b3284ca7a9e2c457
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-21 05:59:41 +0700

    Create getsider.js

---
+++ commands/group/getsider.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch')
+
+module.exports = {
+name: ["getsider"],
+type: ['group'],
+description: "get sider member group",
+admin: true,
+botAdmin:true,
+utilisation: userbot.prefix+ "getsider,
+
+async execute(m) {
+let { conn, text } = data
+
+if (!m.quoted) return m.reply('Reply pesan!')
+  if (!m.quoted.fromMe) throw false
+  if (!m.quoted.id) throw false
+  let members = m.quoted.chat.endsWith('g.us') ? (await conn.groupMetadata(m.quoted.chat)).participants.length - 1 : m.quoted.chat.endsWith('@broadcast') ? -1 : 1
+  let { reads, deliveries } = await conn.messageInfo(m.quoted.chat, m.quoted.id)
+  let txt = `
+*DIBACA OLEH:*
+${reads.sort((a, b) => b.t - a.t).map(({ jid, t }) => `@${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
+${members > 1 ? `${members - reads.length} remaining` : ''}
+
+*TERKIRIM KE:*
+${deliveries.sort((a, b) => b.t - a.t).map(({ jid, t }) => `wa.me/${jid.split`@`[0]}\n_${formatDate(t * 1000)}_`).join('\n')}
+${members > 1 ? `${members - reads.length - deliveries.length} remaining` : ''}
+`.trim()
+  m.reply(txt, null, {
+    contextInfo: {
+      mentionedJid: conn.parseMention(txt)
+    }
+  })
+}
+}
