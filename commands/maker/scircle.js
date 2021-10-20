45594a3143eb43082f3a0f62041688db4d16e68d
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-20 16:44:59 +0700

    Create scircle.js

---
+++ commands/maker/scircle.js
@@ -0,0 +1,21 @@
+const fs = Ft.fs
+const fetch = require("node-fetch")
+var { Canvas } = require("canvacord")
+
+module.exports = {
+name: ["scircle"],
+type: ["maker"],
+useLimit: true,
+description: "make sticker circle",
+utilisation: userbot.prefix + "scircle",
+
+let qe = m.quoted ? m.quoted : m
+  let mime = (qe.msg || qe).mimetype || ""
+  if (/image/.test(mime)) {
+  let img = await qe.download();
+  Canvas.circle(img).then((p) =>
+  let stiker = await sticker(false, p, userbot.packname, userbot.author))
+     conn.sendMessage(m.chat, stiker, 'stickerMessage', {quoted: m})
+  } else return m.reply('reply image')
+}
+}
