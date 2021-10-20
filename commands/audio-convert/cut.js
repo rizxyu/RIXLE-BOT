c5772a3a769512be27046acc9f219aada6b4b73c
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-21 06:28:54 +0700

    Create cut.js

---
+++ commands/audio-convert/cut.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const { exec } = require('child_process')
+
+module.exports = {
+name: ["cut"],
+type: ["audio"],
+useLimit: true,
+description: "cut audio music",
+utilisation: userbot.prefix + "cut",
+
+async execute(m) {
+let { conn } = data
+try {
+        let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
+        let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
+        if (/audio/.test(mime)) {
+            let media = await conn.downloadAndSaveMediaMessage(q)
+            let ran = getRandom('.mp3')
+            exec(`ffmpeg -ss 60 -i ${media} -t 15 -c copy ${ran}`, (err, stderr, stdout) => {
+                fs.unlinkSync(media)
+                if (err) m.reply(`_*Error!*_`)
+                let buff = fs.readFileSync(ran)
+                conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
+                fs.unlinkSync(ran)
+            })
+        } else m.reply(`Balas vn/audio yang ingin diubah dengan caption *${userbot.prefix}cut*`)
+    } catch (e) {
+        throw e
+    }
+}
+}
+
+const getRandom = (ext) => {
+    return `${Math.floor(Math.random() * 10000)}${ext}`
+}
