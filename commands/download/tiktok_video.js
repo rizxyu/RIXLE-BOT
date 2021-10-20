7bc28cecd27d4576feac9a5fadb4d7f216dcd11d
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2021-10-20 10:32:39 +0700

    Fix

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -15,8 +15,7 @@ if (!args[0].includes("tiktok")) return m.replu("url salah!")
 if (!args[1]) return m.reply("tambahkan format wm atau nowm")
 if (!args[1].includes("wm")) return m.reply("format salah")
 download = await tiktok([args[0]])
-_download = download.result[args[1]]
-conn.sendFile(m.chat, _download, "", null, m)
+conn.sendMessage(m.chat, await (await Ft.fetch(download.result[args[1]])).buffer(),"videoMessage")
 } catch (e) {
 m.reply(Ft.util.format(e))
 }
