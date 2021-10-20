b34054500176049162911acf505107c34f12fd3e
SanzGantengz <81704601+SanzGantengz@users.noreply.github.com>
2021-10-20 08:19:39 +0700

    Create semoji.js

---
+++ commands/maker/semoji.js
@@ -0,0 +1,32 @@
+module.exports = {
+  name: ["semoji"],
+  type: ["maker"],
+  description: "make stickers from emoji",
+  utilisation: userbot.prefix+"semoji",
+  async execute(m){
+    let { conn, args } = data;
+    if(!args[0]) return m.reply("masukan parameter emoji")
+    if(!args[1]) return m.reply(`*List yang tersedia:*
+    - apple
+    - google
+    - microsoft
+    - whatsapp
+    - twitter
+    - facebook
+    - jooxpixle
+    - openemoji
+    - emojindex
+    - messanger
+    - lg
+    - htc
+    - mozilla
+    - softbank
+    - docomo
+    - kddi
+    `)
+    await scrap.emoji(args[0])
+     .then(res => res[args[1]].toLowerCase())
+       .then(url => require("../../Lib/converter").stickUrl(url))
+          .then(({ path }) => conn.sendFile(m.chat,Ft.fs.readFileSync(path),"stickerMessage",m)).catch(console.error)
+  }
+}
