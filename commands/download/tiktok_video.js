bd3909e109b1e07c39084355980bd6bc9f14be57
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-20 20:39:42 +0700

    Update tiktok_video.js

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -11,11 +11,9 @@ async execute(m) {
 let { args } = data
 try {
 if (!args[0]) return m.reply("url ?")
-if (!args[0].includes("tiktok")) return m.replu("url salah!")
-if (!args[1]) return m.reply("tambahkan format wm atau nowm")
-if (!args[1].includes("wm")) return m.reply("format salah")
-download = await tiktok([args[0]])
-conn.sendMessage(m.chat, await (await Ft.fetch(download.result[args[1]])).buffer(),"videoMessage",{quoted:m})
+conn.send3Button(m.chat, `SILAHKAN PILIH TIPE YG AKAN DIDOWNLOAD!`, userbot.prefix, 'Wm', `twm ${[args[0]]}`, `NoWm`, `tnowm ${[args[0]]}`, `Music` , `tmusic ${[args[0]]}`, {quoted: m})
+//download = await tiktok([args[0]])
+//conn.sendMessage(m.chat, await (await Ft.fetch(download.result[args[1]])).buffer(),"videoMessage",{quoted:m})
 } catch (e) {
 m.reply(Ft.util.format(e))
 }
