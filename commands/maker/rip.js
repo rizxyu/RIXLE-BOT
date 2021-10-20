fa7fdbe28501c5d43639ea3208bf97cef86e2399
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-20 07:10:17 +0700

    Update rip.js

--- commands/maker/rip.js
+++ commands/maker/rip.js
@@ -16,9 +16,9 @@ const name = conn.getName(m.sender)
 if (m.quoted.sender) {
 Canvas.rip(await conn.getProfilePicture(m.quoted.sender)).then((p) => 
 conn.sendFile(m.chat, p, 'p.jpg', `Rip ${conn.getName(m.quoted.sender)}`, null, m))
-} else if (m.sender) {
-Canvas.rip(await conn.getProfilePicture(m.sender)).then((p) => 
-conn.sendFile(m.chat, p, 'p.jpg', `Rip ${conn.getName(m.sender)}`, null, m))
+} else if (conn.user.jid) {
+Canvas.rip(await conn.getProfilePicture(conn.user.jid)).then((p) => 
+conn.sendFile(m.chat, p, 'p.jpg', `Rip ${conn.getName(conn.user.jid)}`, null, m))
 }
 }
 }
