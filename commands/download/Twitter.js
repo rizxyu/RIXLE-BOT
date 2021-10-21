eafcb9a2c57b6a9fb532b10403215ec175ec2ae7
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-21 09:55:35 +0700

    Create Twitter.js

---
+++ commands/download/Twitter.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch')
+
+module.exports = {
+name: ["twitter"],
+type: ["download"],
+useLimit: true,
+description: "download audio from twitter url",
+utilisation: userbot.prefix + "twitter <link>",
+
+async execute(m) {
+let { conn, args } = data
+if (!args[0]) return m.reply("url nya om?")
+
+let res = await fetch (`https://rizapi.herokuapp.com/api/twitter?url=${args}`)
+let json = await res.json()
+
+conn.send3ButImg(m.chat, json.result.thumb, `*Twitter Downloader*\n Pilih type dibawah`, userbot.prefix, `HD`, `thd ${args[0]}`, `SD`, `tsd ${args[0]}`, `AUDIO`, `taudio ${args[0]}`, m)
+}
+}
