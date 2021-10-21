09464882821a83ae8f136341d8cc08a1ff84b155
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-21 12:17:05 +0700

    Fix

--- commands/group/tagall.js
+++ commands/group/tagall.js
@@ -5,13 +5,16 @@ type: ["group"],
 description: "tag",
 admin: true,
 utilisation: "#tagall",
-async execute(m, member) {
+async execute(m) {
 let { conn, text } = data
 
 if (!text) return m.reply('Where text')
-let users = member.participants.map(u => u.jid)
-  m.reply(text + '\n' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n`, null, {
-    contextInfo: { mentionedJid: users }
-  })
+conn.fetchGroupMetadataFromWA(m.chat).then(({ participants }) => {
+let a = "anu\n\n"
+for (i of participants){
+a += `@${i.jid.split("@")[0]}\n`
+}
+m.reply(`*TAGALL*\n\nInfo: ${text}\n\n${a}`)
+})
 }
 }
