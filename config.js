6457133f6450d0e9a66b761ee1499f916a2a9876
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-10-22 06:23:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 userbot = {
- owner: [ "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
+ owner: [ "6281261324817", "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
   MONGO_URI: "mongodb+srv://zanDb:veth0581@cluster0.eyx0e.mongodb.net/<mydb>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
