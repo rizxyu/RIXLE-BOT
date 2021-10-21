c825e7c3a22948ad5d396111e9d49372d06f8912
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-21 09:08:52 +0700

    update hug.js

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -10,24 +10,8 @@ let { conn } = data
 if (m.quoted && m.quoted.sender) m.mentionedJid.push(m.quoted.sender);
 if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
 let res = await fetch(`https://some-random-api.ml/animu/hug`)
-if (!res.ok) return m.reply(`${res.status} ${res.statusText}`);
 	let json = await res.json();
-	if (json.link)
-		conn.sendFile(
-			m.chat,
-			json.link,
-			"wibu.gif",
-			`@${m.sender.split("@")[0]} ${command} ${m.mentionedJid
-				.map((user) =>
-					user === m.sender ? "themselves " : `@${user.split("@")[0]}`
-				)
-				.join(", ")}`,
-			m,
-			false,
-			{
-				asGIF: true,
-				contextInfo: {
-					mentionedJid: [...m.mentionedJid, m.sender],
+conn.sendFile(m.chat, json.link, "wibu.gif", `@${m.sender.split("@")[0]} ${command} ${m.mentionedJid.map((user) => user === m.sender ? "themselves " : `@${user.split("@")[0]}`).join(", ")}`,m, { contextInfo: { mentionedJid: [...m.mentionedJid, m.sender],
 				},
 			}
 		);
