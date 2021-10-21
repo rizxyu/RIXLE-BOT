dffb62a1aaa44509349ffe27a2f0240431e02e93
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-21 08:58:57 +0700

    Create hug.js

---
+++ commands/fun/hug.js
@@ -0,0 +1,36 @@
+let fetch = require("node-fetch");
+module.exports = {
+name: ["hug"],
+type: ["fun"],
+description: "screenshot web link",
+utilisation: userbot.prefix + "ssweb <link>",
+
+async execute(m) {
+let { conn } = data
+if (m.quoted && m.quoted.sender) m.mentionedJid.push(m.quoted.sender);
+if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
+let res = await fetch(`https://some-random-api.ml/animu/hug`)
+if (!res.ok) return m.reply(`${res.status} ${res.statusText}`);
+	let json = await res.json();
+	if (json.link)
+		conn.sendFile(
+			m.chat,
+			json.link,
+			"wibu.gif",
+			`@${m.sender.split("@")[0]} ${command} ${m.mentionedJid
+				.map((user) =>
+					user === m.sender ? "themselves " : `@${user.split("@")[0]}`
+				)
+				.join(", ")}`,
+			m,
+			false,
+			{
+				asGIF: true,
+				contextInfo: {
+					mentionedJid: [...m.mentionedJid, m.sender],
+				},
+			}
+		);
+	else return m.reply(json);
+}
+}
