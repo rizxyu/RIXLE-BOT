afff3357cbb5f6fd4021ed7b1bf2798842182c10
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-21 19:21:00 +0700

    Create google.js

---
+++ commands/audio-convert/google.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const { exec } = require('child_process')
+
+module.exports = {
+name: ["gg"],
+type: ["audio"],
+useLimit: true,
+description: "vn/audio to convert like google sound",
+utilisation: userbot.prefix + "gg",
+
+async execute(m) {
+let { conn, args } = data
+try {
+if (!args[0]) return m.reply('dari detik?')
+if (!args[1]) return m.reply('ke detik?')
+        let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
+        let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
+        if (/audio/.test(mime)) {
+            let media = await conn.downloadAndSaveMediaMessage(q)
+            let ran = getRandom('.mp3')
+            exec(`ffmpeg -i ${media} -ar 48000 -vn -c:a libopus ${ran}`, (err, stderr, stdout) => {
+                fs.unlinkSync(media)
+                if (err) m.reply(`_*Error!*_`)
+                let buff = fs.readFileSync(ran)
+                conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
+                fs.unlinkSync(ran)
+            })
+        } else m.reply(`Balas vn/audio yang ingin diubah dengan caption *${userbot.prefix}gg*`)
+    } catch (e) {
+        throw e
+    }
+}
+}
+//Punya mamang rizky
+const getRandom = (ext) => {
+    return `${Math.floor(Math.random() * 10000)}${ext}`
+}
