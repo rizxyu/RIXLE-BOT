241417f49f55a030ffe23b6e3245e23dbaaeccdb
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-22 20:23:49 +0700

    Update absen_mulai.js

--- commands/group/absen_mulai.js
+++ commands/group/absen_mulai.js
@@ -8,11 +8,12 @@ let { conn, text } = data
 conn.absen = conn.absen ? conn.absen : {}
     let id = m.chat
     if (id in conn.absen) {
-        await conn.sendButton(m.chat, `Masih ada absen di chat ini!\n\nketik *${usedPrefix}hapusabsen* untuk menghapus absen`.trim(), '© stikerin', 'Hapus', `${usedPrefix}hapusabsen`, conn.absen[id][0])
+        await conn.sendButton(m.chat, `Tidak ada absen berlangsung digrup ini!`, userbot.packname, 'Mulai', 'absenm', {quoted: m})
         throw false
     }
     conn.absen[id] = [
-        await m.reply(`Absen dimulai silahkan ketik #absen`),
+        await conn.sendButton(m.chat, caption, userbot.prefix, 'Cekabsen', 'absenc', { quoted: m, contextInfo: {"mentionedJid": conn.parseMention(caption)}} )
+ 
         [],
         text
     ]
