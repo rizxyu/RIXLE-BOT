1ec488cd63a24cd85d563beefb6015aa77d204ea
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-22 08:13:59 +0700

    Update broadcast.js

--- commands/owner/broadcast.js
+++ commands/owner/broadcast.js
@@ -19,7 +19,7 @@ utilisation: userbot.prefix + "bc <args>",
   
  for ( let id of chats) {
    await delay(1500)
-   await conn.sendButtonLoc( m.chat, await ( await fetch('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa1pVfdb1zUoSve4Unc08jl5BpCHwfys8qxA&usqp=CAU')).buffer(), `${teks}\n\n[ BROADCAST ]`, `Rixle Bot By Rizky Adi`, `CREATOR`, `creator`, m)
+   await conn.sendButtonLoc(id.jid, await ( await fetch('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa1pVfdb1zUoSve4Unc08jl5BpCHwfys8qxA&usqp=CAU')).buffer(), `${teks}\n\n[ BROADCAST ]`, `Rixle Bot By Rizky Adi`, `CREATOR`, `creator`, m)
     }
     m.reply('BROADCAST SELESAI')
   }
