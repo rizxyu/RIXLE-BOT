322a437804131c16c8907972f7e3b490f79108d3
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-22 14:38:16 +0700

    Update reverb.js

--- commands/audio-convert/reverb.js
+++ commands/audio-convert/reverb.js
@@ -14,14 +14,16 @@ try {
         let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
         let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
         if (/audio/.test(mime)) {
-            let media = await conn.downloadAndSaveMediaMessage(q)
+            let filename = path.join(__dirname, '../tmp/' + ran)
+            let media = await conn.downloadAndSaveMediaMessage(q, filename)
             let ran = getRandom('.mp3')
+            
             exec(`ffmpeg -i ${media} -i ${media} -filter_complex '[0] [1] afir=dry=10:wet=10' ${ran}`, (err, stderr, stdout) => {
                 fs.unlinkSync(media)
-                if (err) m.reply(`_*Error!*_`)
-                let buff = fs.readFileSync(ran)
-                conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio' })
-                fs.unlinkSync(ran)
+                if (err) m.reply(`_*TERJADI KESALAHAN!*_`)
+                let buff = fs.readFileSync(filename)
+                conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
+                await fs.unlinkSync(filename)
             })
         } else m.reply(`Balas vn/audio yang ingin diubah dengan caption *${userbot.prefix}reverb*`)
     } catch (e) {
