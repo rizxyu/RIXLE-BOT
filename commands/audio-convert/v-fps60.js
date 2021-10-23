f201497d41f07558a6785a19303648804d61a614
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-23 19:59:27 +0700

    Update v-fps60.js

--- commands/audio-convert/v-fps60.js
+++ commands/audio-convert/v-fps60.js
@@ -20,13 +20,14 @@ try {
         let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
         let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
         if (/video/.test(mime)) {
-            let media = await conn.downloadAndSaveMediaMessage(q)
+            let filename = path.join(__dirname, '../tmp/' + ran)
+            let media = await conn.downloadAndSaveMediaMessage(q, filename)
             let ran = getRandom('.mp4')
             exec(`ffmpeg -i ${media} -y -filter:v "minterpolate='mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=60'" ${ran}`, (err, stderr, stdout) => {
                 fs.unlinkSync(media)
                 if (err) m.reply(`_*Error!*_`)
-                let buff = fs.readFileSync(ran)
-                conn.sendFile(m.chat, buff, null, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
+                let buff = fs.readFileSync(filename)
+                conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
                 fs.unlinkSync(ran)
             })
         } else m.reply(`Balas vn/audio yang ingin diubah dengan caption *${userbot.prefix}slow*`)
