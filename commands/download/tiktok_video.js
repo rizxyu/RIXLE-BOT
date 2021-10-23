2ca76a22d10913a3461834ca0aaf0be9086618ec
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-10-23 12:15:49 +0700

    Update tiktok_video.js

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -2,7 +2,7 @@ const fs = Ft.fs
 const tiktok = require("../../Lib/scrape.js").tiktok
 
 module.exports = {
-name: ["tiktok"],
+name: ["tiktok", "tt"],
 type: ["download"],
 description: "download video from tiktok with url",
 utilisation: userbot.prefix + "tiktok <link>",
@@ -11,7 +11,7 @@ async execute(m) {
 let { args } = data
 try {
 if (!args[0]) return m.reply("url ?")
-conn.send2Button(m.chat, `Pilih Type Barikut`, userbot.packname,`No wm`, `tnowm ${[args[0]]}`, `Music` , `tmusic ${[args[0]]}`, {quoted: m})
+conn.send2Button(m.chat, `Pilih Type Barikut ${m.mention}`, userbot.packname,`MP4`, `tnowm ${[args[0]]}`, `MP3` , `tmusic ${[args[0]]}`, {quoted: m})
 //download = await tiktok([args[0]])
 //conn.sendMessage(m.chat, await (await Ft.fetch(download.result[args[1]])).buffer(),"videoMessage",{quoted:m})
 } catch (e) {
