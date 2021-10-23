4d5f58579a1b6cfe247269f21262980989a37f8c
JJ <77143046+Oreki-san@users.noreply.github.com>
2021-10-23 13:41:40 +0530

    Create kiss.js

---
+++ commands/fun/kiss.js
@@ -0,0 +1,18 @@
+let fetch = require("node-fetch");
+module.exports = {
+name: ["kiss"],
+type: ["fun"],
+description: "screenshot web link",
+utilisation: userbot.prefix + "ssweb <link>",
+
+async execute(m) {
+let { conn } = data
+if (m.quoted && m.quoted.sender) m.mentionedJid.push(m.quoted.sender);
+if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
+let res = await fetch(`https://waifu.pics/sfw/kiss`)
+	let json = await res.json();
+conn.sendFile(m.chat, json.link, "wibu.gif", `@${m.sender.split("@")[0]} ${command} ${m.mentionedJid.map((user) => user === m.sender ? "themselves " : `@${user.split("@")[0]}`).join(", ")}`, m, { contextInfo: { mentionedJid: [...m.mentionedJid, m.sender],
+				}})
+	//else return m.reply(json);
+}
+}
