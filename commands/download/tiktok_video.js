e52831cc4fc6d542aea50e1dc29d257432176eba
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-23 19:47:26 +0700

    Update tiktok_video.js

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -1,7 +1,7 @@
 const fs = Ft.fs
 const tiktokmusic = require("../../Lib/scrape.js").tiktokmusic
 const tiktok = require("../../Lib/scrape.js").tiktok
-
+const fetch = require('node-fetch')
 module.exports = {
 name: ["tiktok", "tt"],
 type: ["download"],
@@ -15,7 +15,7 @@ if (!args[0]) return m.reply("please input url")
 let ttdata = await tiktokmusic(`${[args[0]]}`)
 let teks = `Nickname : ${ttdata.meta.author.nickname}\nDesc : ${ttdata.meta.desc}\nDuration : ${ttdata.meta.video.duration}\n\nPilih Type Dibawah Ini ${m.mention}`
 
-conn.send2ButImg(m.chat, teks,await Ft.getBuffer(ttdata.meta.video.cover),userbot.packname,`MP4`, `tnowm ${[args[0]]}`, `MP3` , `tmusic ${[args[0]]}`, {quoted: m, contextInfo: {"mentionedJid": conn.parseMention(teks) }})
+conn.send2ButImg(m.chat, await (await fetch(ttdata.meta.video.cover)).buffer(), teks,userbot.packname,`MP4`, `tnowm ${[args[0]]}`, `MP3` , `tmusic ${[args[0]]}`, {quoted: m})
 } catch (e) {
 m.reply(Ft.util.format(e))
 }
