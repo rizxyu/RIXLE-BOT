c43c80ad44715184fc48e156b3990a6c53fa592e
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-10-23 19:17:23 +0700

    Update tiktok_video.js

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -1,4 +1,5 @@
 const fs = Ft.fs
+const tiktokmusic = require("../../Lib/scrape.js").tiktokmusic
 const tiktok = require("../../Lib/scrape.js").tiktok
 
 module.exports = {
@@ -10,11 +11,14 @@ utilisation: userbot.prefix + "tiktok <link>",
 async execute(m) {
 let { args } = data
 try {
-if (!args[0]) return m.reply("url ?")
-let teks = `Pilih Type Barikut ${m.mention}`
-conn.send2Button(m.chat, teks, userbot.packname,`MP4`, `tnowm ${[args[0]]}`, `MP3` , `tmusic ${[args[0]]}`, {quoted: m, contextInfo: {"mentionedJid": conn.parseMention(teks) }})
-//download = await tiktok([args[0]])
-//conn.sendMessage(m.chat, await (await Ft.fetch(download.result[args[1]])).buffer(),"videoMessage",{quoted:m})
+if (!args[0]) return m.reply("please input url")
+if (!args[0].includes("tiktok.com/")) return m.reply("url unvalid")
+let ttdata = await tiktokmusic(url)
+let teks = `Nickname : ${ttdata.meta.author.nickname}`
+teks += `Desc : ${ttdata.meta.desc}`
+teks += `Duration : ${ttdata.meta.video.duration}\n`
+teks += `Pilih Type Dibawah Ini ${m.mention}`
+conn.send2ButtonImg(m.chat, teks, ttdata.meta.cover,userbot.packname,`MP4`, `tnowm ${[args[0]]}`, `MP3` , `tmusic ${[args[0]]}`, {quoted: m, contextInfo: {"mentionedJid": conn.parseMention(teks) }})
 } catch (e) {
 m.reply(Ft.util.format(e))
 }
