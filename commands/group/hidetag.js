8abaf31cbf37b43fa2136371ef278c0e80a1e1d8
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-23 11:39:23 +0700

    Create hidetag.js

---
+++ commands/group/hidetag.js
@@ -0,0 +1,23 @@
+
+let { MessageType, mentionedJid } = require("@adiwajshing/baileys")
+
+module.exports = {
+name: ["hidetag","everyone],
+type: ['group'],
+description: "menendang member grup(hanya boleh dilakukan ketika anda menjadi admin)",
+admin: true,
+utilisation: global.userbot.prefix+ "hidetag","everyone",
+
+async execute(m) {
+let { conn, text } = data
+if (!text) return m.reply('teks untuk hidetag?')
+
+ conn.fetchGroupMetadataFromWA(m.chat).then(({ participants }) => {
+let ane = []
+	       for (let i of participants){
+	       ane.push(i.jid)
+}
+conn.sendMessage(m.chat, text, MessageType.text, { quoted: m, contextInfo: {"mentionedJid": ane}})
+})
+}
+}
