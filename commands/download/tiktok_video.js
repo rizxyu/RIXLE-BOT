9286735413195e65d42ed0dfabece03e474c25ba
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-10-23 20:23:35 +0700

    Update tiktok_video.js

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -11,3 +11,5 @@ let url = args[0]
 let ttdata = await tiktokmusic(url)
 let teks = `*Nickname :* ${ttdata.meta.author.nickname}\n*Desc :* ${ttdata.meta.desc}\n*Duration* : ${ttdata.meta.video.duration}\n\n_Pilih Type Dibawah Ini_ ${m.mention}`
 conn.send2ButtonImg(m.chat, teks, await Ft.getBuffer(ttdata.meta.video.cover), userbot.packname, "MP3","tmusic "+url, "MP4", "ttnowm "+url, {contextInfo:{"mentionedJid": conn.parseMention(teks)}})
+}
+}
