bdb3f39850de2fa0a002f3c9b865925504c61dc1
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-10-23 12:17:36 +0700

    Update tiktok_video.js

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -11,7 +11,7 @@ async execute(m) {
 let { args } = data
 try {
 if (!args[0]) return m.reply("url ?")
-conn.send2Button(m.chat, `Pilih Type Barikut ${m.mention}`, userbot.packname,`MP4`, `tnowm ${[args[0]]}`, `MP3` , `tmusic ${[args[0]]}`, {quoted: m})
+conn.send2Button(m.chat, `Pilih Type Barikut ${m.mention}`, userbot.packname,`MP4`, `tnowm ${[args[0]]}`, `MP3` , `tmusic ${[args[0]]}`, {quoted: m, contextInfo: {"mentionedJid": m.mention }})
 //download = await tiktok([args[0]])
 //conn.sendMessage(m.chat, await (await Ft.fetch(download.result[args[1]])).buffer(),"videoMessage",{quoted:m})
 } catch (e) {
