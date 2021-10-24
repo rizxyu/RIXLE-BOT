87b242f11a42727ed8adbf429785ca26a8285286
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-24 16:43:58 +0700

    Update action.js

--- action/action.js
+++ action/action.js
@@ -1,4 +1,5 @@
 let { MessageType, mentionedJid } = require("@adiwajshing/baileys")
+let knights = require('knights-canvas')
 let fetch = Ft.fetch
 module.exports = {
 async battery(json) {
@@ -26,9 +27,25 @@ img = await (await fetch('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9Gc
 }
 switch (action) {
 case 'remove': 
+  let pp = 'https://ibb.co/syWXQ7J'
+  let ppgc = 'https://ibb.co/syWXQ7J'
+try {
+  pp = await uploadImage(await (await fetch(await this.getProfilePicture(user))).buffer())
+   ppgc = await uploadImage(await (await fetch(await this.getProfilePicture(jid))).buffer())
+            } catch (e) {
+            } finally {
+  let wel = await new knights.Welcome()
+                .setUsername(this.getName(user))
+                .setGuildName(this.getName(jid))
+                .setGuildIcon(ppgc)
+                .setMemberCount(groupM.participants.length)
+                .setAvatar(pp)
+                .setBackground("https://i.ibb.co/KhtRxwZ/dark.png")
+                .toAttachment()
+                
 teks = `@${mem.split("@")[0]} Keluar Dari Group ${groupM.subject}`
-conn.sendMessage(member.jid, teks, MessageType.text, {thumbnail: img,
-contextInfo: {"mentionedJid": conn.parseMention(teks)}})
+conn.sendFile(member.jid, wel.toBuffer(), teks, { quoted: m, contextInfo: {"mentionedJid": conn.parseMention(teks)}})
+}
 break
 case 'add' : 
 teks = `@${mem.split("@")[0]} Bergabung ke Dalam Group ${groupM.subject}`
