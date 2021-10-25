ce08ffc5cd0436fb7f8c52f700c285035f1ea31d
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-10-25 13:23:55 +0700

    Update tiktok_video.js

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -3,16 +3,15 @@ module.exports = {
 name: ["tiktok", "tt"],
 type: ["download"],
 description: "download video from tiktok with url",
-utilisation: userbot.prefix + "tiktok <link>",
+utilisation: userbot.prefix + "tiktok <url>",
 
 async execute(m) {
-let { text } = data
-let url = m.quoted.text || text;
-//if ( m.quoted.text) return m.reply('reply textnya')
+let { args } = data
+let url = m.quoted.text || args[ 0]
 if (!url) return m.reply("please input url")
 try {
 let ttdata = await tiktokmusic(url)
-let teks = `*Nickname :* ${ttdata.meta.author.nickname}\n*Desc :* ${ttdata.meta.desc}\n*Duration* : ${ttdata.meta.video.duration}\n\n_Pilih Type Dibawah Ini_ ${m.mention}`
+let teks = `*Nickname :* ${ttdata.meta.author.nickname}\n*Desc :* ${ttdata.meta.desc}\n*Duration* : ${ttdata.meta.video.duration}\n\n_Pilih Type Dibawah Ini_  ${m.mention}`
 conn.send2ButtonImg(m.chat, teks, await Ft.getBuffer(ttdata.meta.video.cover), userbot.packname, "MP3","tmusic "+url, "MP4", "tnowm "+url, {contextInfo:{"mentionedJid": conn.parseMention(teks)}})
 } catch (e) {
 m.reply(Ft.util.format(e))
