fa8d5bc102b7d4eb69d82472ffe43ac2c182766a
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-10-26 14:27:54 +0700

    Update rgb.js

--- commands/audio-convert/rgb.js
+++ commands/audio-convert/rgb.js
@@ -5,7 +5,7 @@ module.exports = {
 name: ["rgb"],
 type: ["audio"],
 useLimit: true,
-description: "convert audio normal to rgb",
+description: "convert video normal to rgb",
 utilisation: userbot.prefix + "rgb reply",
 
 async execute(m) {
@@ -13,17 +13,17 @@ let { conn, args } = data
 try {
         let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
         let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
-        if (/audio/.test(mime)) {
+        if (/video/.test(mime)) {
             let media = await conn.downloadAndSaveMediaMessage(q)
-            let ran = getRandom('.mp3')
+            let ran = getRandom('.mp4')
             exec(`ffmpeg -i ${media} -vf extractplanes=r -c:v libx264rgb -pix_fmt rgb24 ${ran}`, (err, stderr, stdout) => {
                 fs.unlinkSync(media)
                 if (err) m.reply(`_*Error!*_`)
                 let buff = fs.readFileSync(ran)
-                conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
+                conn.sendFile(m.chat, buff, ran, null, m, { quoted: m, mimetype: 'video/mp4' })
                 fs.unlinkSync(ran)
             })
-        } else m.reply(`Balas vn/audio yang ingin diubah dengan caption *${userbot.prefix}bass*`)
+        } else m.reply(`Balas video yang ingin diubah dengan caption *${userbot.prefix}rgb*`)
     } catch (e) {
         throw e
     }
