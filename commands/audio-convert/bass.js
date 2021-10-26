85042b39a83d84977121295b45475e49818d0548
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-26 13:30:50 +0700

    Update bass.js

--- commands/audio-convert/bass.js
+++ commands/audio-convert/bass.js
@@ -12,12 +12,14 @@ async execute(m) {
 let { conn, args } = data
 try {
         if (!args[0]) return m.reply('masukkan angka')
+       if (!args[1]) return m.reply('masukkan angka lagi')
+  
         let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
         let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
         if (/audio/.test(mime)) {
             let media = await conn.downloadAndSaveMediaMessage(q)
             let ran = getRandom('.mp3')
-            exec(`ffmpeg -i ${media} -af equalizer=f=${args[0]}:width_type=o:width=1:g=10 ${ran}`, (err, stderr, stdout) => {
+            exec(`ffmpeg -i ${media} -af equalizer=f=${args[0]}:width_type=o:width=1:g=${args[1]} ${ran}`, (err, stderr, stdout) => {
                 fs.unlinkSync(media)
                 if (err) m.reply(`_*Error!*_`)
                 let buff = fs.readFileSync(ran)
