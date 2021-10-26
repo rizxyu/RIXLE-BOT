d16fad1f72f1333eedd0ed6f8ebc5b27d9ae0605
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-10-26 14:31:49 +0700

    Update rgb.js

--- commands/audio-convert/rgb.js
+++ commands/audio-convert/rgb.js
@@ -13,14 +13,14 @@ let { conn, args } = data
 try {
         let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
         let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
-        if (/video/.test(mime)) {
+        if (/image/.test(mime)) {
             let media = await conn.downloadAndSaveMediaMessage(q)
-            let ran = getRandom('.mp4')
+            let ran = getRandom('.jpg')
             exec(`ffmpeg -i ${media} -vf extractplanes=r -c:v libx264rgb -pix_fmt rgb24 ${ran}`, (err, stderr, stdout) => {
                 fs.unlinkSync(media)
                 if (err) m.reply(`_*Error!*_`)
                 let buff = fs.readFileSync(ran)
-                conn.sendFile(m.chat, buff, ran, null, m, { quoted: m, mimetype: 'video/mp4' })
+                conn.sendFile(m.chat, buff, ran, null, m, { quoted: m, mimetype: 'image/jpeg' })
                 fs.unlinkSync(ran)
             })
         } else m.reply(`Balas video yang ingin diubah dengan caption *${userbot.prefix}rgb*`)
