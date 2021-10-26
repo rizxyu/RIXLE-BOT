c039fd5dd6160730b29df8f15e9b1b241fc92079
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-26 11:20:42 +0700

    Create changelog.js

---
+++ commands/core/changelog.js
@@ -0,0 +1,13 @@
+module.exports = {
+name: ["changelog"],
+type: ["default"],
+description: "changelog bot",
+utilisation: userbot.prefix + "changelog",
+
+async execute(m) {
+let { conn, text } = data
+
+m.reply(userbot.chalog)
+
+}
+}
