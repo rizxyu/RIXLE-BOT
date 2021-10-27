59d6dff1ba851a0bc81f98b7de6da9a5d5ba42ce
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-10-27 11:12:33 +0700

    Update antilinkgroup.js

--- commands/function/antilinkgroup.js
+++ commands/function/antilinkgroup.js
@@ -3,13 +3,11 @@ name: "antilinkgroup", //@arifirazzaq2001
 admin: true,
 botAdmin: true,
 
-async functions(m) { 
+async functions(m) {
 let { conn } = data
  if (!m.sender == conn.user.jid && m.text.includes("chat.whatsapp.com")) {
 m.reply("Maaf Anda Di Keluarkan")
 await conn.groupRemove(m.chat, [m.quoted.sender])
 }
 }
-}
-
-
+} //gantian buat switch nya, gw dah buat rangkaian nya
