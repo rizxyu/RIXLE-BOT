694085bc2c065161a3780106199ee7847be24c42
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-02 19:10:47 +0700

    Update antilinkgroup.js

--- commands/function/antilinkgroup.js
+++ commands/function/antilinkgroup.js
@@ -1,5 +1,5 @@
 module.exports = { 
-name: "antilinkgroup", //@arifirazzaq2001 Fixed by Ivanzz`
+name: "antilinkgroup", //By @arifirazzaq2001 Fixed by Ivanzz`
 async functions(m) { 
 const gMdata = m.isGroup ? await conn.groupMetadata(from) : ''
 const grupAdmin = m.isGroup ? getGroupAdmin(gMdata.participants) : ''
@@ -7,7 +7,7 @@ const isAdmin = grupAdmin.includes(m.sender)
  if (m.message || m.text.includes("https://chat.whatsapp.com/")) {
  if (m.sender === conn.user.jid) return
  if (m.sender === isAdmin) return
-m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Group Terdeteksi!* \n_Anda Di Keluarkan._`)
+m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Group Terdeteksi!* \n_Maaf Anda Di Keluarkan._`)
 await conn.groupRemove(m.chat, [m.sender])
 }
 }
