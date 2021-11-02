bb4f11380be3422c796134983949ac8219b573ca
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-02 19:13:25 +0700

    Create antilinkytchannel.js

---
+++ commands/function/antilinkytchannel.js
@@ -0,0 +1,22 @@
+module.exports = { 
+name: "antilinkytchannel", //By @arifirazzaq2001
+async functions(m) { 
+const gMdata = m.isGroup ? await conn.groupMetadata(from) : ''
+const grupAdmin = m.isGroup ? getGroupAdmin(gMdata.participants) : ''
+const isAdmin = grupAdmin.includes(m.sender)
+ if (m.message || m.text.includes("https://youtube.com/channel/")) {
+ if (m.sender === conn.user.jid) return
+ if (m.sender === isAdmin) return
+m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Youtube Channel Terdeteksi!* \n_Maaf Anda Di Keluarkan._`)
+await conn.groupRemove(m.chat, [m.sender])
+}
+}
+}
+
+function getGroupAdmin(participants) {
+	admins = []
+	for (let i of participants) {
+		i.isAdmin ? admins.push(i.jid) : ''
+	}
+	return admins
+}
