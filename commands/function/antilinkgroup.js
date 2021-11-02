4c3d426f52589188405bbc7443ebb4ece926c7d2
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-11-02 18:02:59 +0700

    Update antilinkgroup.js

--- commands/function/antilinkgroup.js
+++ commands/function/antilinkgroup.js
@@ -1,14 +1,22 @@
 module.exports = { 
-name: "antilinkgroup", //@arifirazzaq2001
-admin: true,
-botAdmin: true,
-
+name: "antilinkgroup", //@arifirazzaq2001 Fixed by Ivanzz`
 async functions(m) { 
-let { conn } = data
- if (m.message && m.text.includes("https://chat.whatsapp.com")) {
- if (!m.sender == this.user.jid) return false
-m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Group Terdeteksi!* \n_Maaf Anda Di Keluarkan._`)
-await conn.groupRemove(m.chat, [m.quoted.sender])
+const gMdata = m.isGroup ? await conn.groupMetadata(from) : ''
+const grupAdmin = isGroup ? getGroupAdmin(gMdata.participants) : ''
+const isAdmin = grupAdmin.includes(m.sender)
+ if (m.message || m.text.includes("https://chat.whatsapp.com/")) {
+ if (m.sender === conn.user.jid) return
+ if (m.sender === isAdmin) return
+m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Group Terdeteksi!* \n_Anda Di Keluarkan._`)
+await conn.groupRemove(m.chat, [m.sender])
 }
 }
+}
+
+function getGroupAdmin(participants) {
+	admins = []
+	for (let i of participants) {
+		i.isAdmin ? admins.push(i.jid) : ''
+	}
+	return admins
 }
