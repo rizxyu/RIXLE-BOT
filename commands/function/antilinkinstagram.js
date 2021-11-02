8aa7f5bb41fc522f4eb53e439afa75041bbaf54f
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-02 19:14:30 +0700

    Create antilinkinstagram.js

---
+++ commands/function/antilinkinstagram.js
@@ -0,0 +1,24 @@
+module.exports = { 
+name: "antilinkinstagram", //By @arifirazzaq2001
+async functions(m) { 
+const gMdata = m.isGroup ? await conn.groupMetadata(from) : ''
+const grupAdmin = m.isGroup ? getGroupAdmin(gMdata.participants) : ''
+const isAdmin = grupAdmin.includes(m.sender)
+ if (m.message || m.text.includes("https://instagram.com/")) {
+ if (m.message || m.text.includes("https://www.instagram.com/")) {
+ if (m.sender === conn.user.jid) return
+ if (m.sender === isAdmin) return
+m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Instagram Terdeteksi!* \n_Maaf Anda Di Keluarkan._`)
+await conn.groupRemove(m.chat, [m.sender])
+}
+}
+}
+}
+
+function getGroupAdmin(participants) {
+	admins = []
+	for (let i of participants) {
+		i.isAdmin ? admins.push(i.jid) : ''
+	}
+	return admins
+}
