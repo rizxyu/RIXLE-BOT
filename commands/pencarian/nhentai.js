57362a7e306e17be318c552645ab1cdf0b835e61
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-03 16:20:49 +0700

    Create nhentai.js

---
+++ commands/pencarian/nhentai.js
@@ -0,0 +1,22 @@
+const nhentai = require('nhentai')
+const api = new nhentai.API()
+
+module.exports = {
+name: ["nhentai"],
+type: ["anime"],
+description: "search nhentai",
+utilisation: userbot.prefix+"nhentai",
+
+async execute(m){
+let { conn, text } = data
+if (!text) return m.reply('masukkan kode nhentai')
+
+api.fetchDoujin(text).then(doujin => m.reply(`
+🔖Title: ${doujin.titles.pretty;}
+🔗Page url: ${doujin.pages[0].url;}
+🖼️Cover url: ${doujin.cover.url}
+#️⃣tag: ${doujin.tags.all.map(tag => tag.name).join(', ')}
+`))
+
+}
+}
