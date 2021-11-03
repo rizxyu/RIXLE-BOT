871c65004c133e8ee67f4fd069258c25d8bb2e78
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-03 10:11:34 +0700

    full fix @arifirazzaq2001

--- commands/function/antilinkytvideo.js
+++ commands/function/antilinkytvideo.js
@@ -1,13 +1,25 @@
 module.exports = { 
 name: "antilinkytvideo", //By @arifirazzaq2001
-async functions(m) { 
-const gMdata = m.isGroup ? await conn.groupMetadata(from) : ''
+admin: true,
+botAdmin: true,
+
+async functions(m) {
+let { conn } = data
+const botNumber = conn.user.jid
+const gMdata = m.isGroup ? await conn.groupMetadata(m.chat) : ''
 const grupAdmin = m.isGroup ? getGroupAdmin(gMdata.participants) : ''
 const isAdmin = grupAdmin.includes(m.sender)
- if (m.message || m.text.includes("https://youtu.be/")) {
+const groupMembers = m.isGroup ? gMdata.participants : ''
+const groupAdmins = m.isGroup ? getGroupAdmin(groupMembers) : ''
+const isGroupAdmins = groupAdmins.includes(m.sender) || false 
+const isBotGroupAdmins = groupAdmins.includes(botNumber)
+
+ if (m.text.includes("https://youtu.be/")) {
  if (m.sender === conn.user.jid) return
  if (m.sender === isAdmin) return
-m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Youtube Video Terdeteksi!* \n_Maaf Anda Di Keluarkan._`)
+ if (isGroupAdmins) return // Melewati Transisi Ketika Detected Suatu Grup Yang Bukan Admin
+ if (!isBotGroupAdmins) return //Detect Clone Bot Harus Admin
+m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Video Youtube Terdeteksi!* \n_Maaf Anda Di Keluarkan._`)
 await conn.groupRemove(m.chat, [m.sender])
 }
 }
@@ -19,4 +31,4 @@ function getGroupAdmin(participants) {
 		i.isAdmin ? admins.push(i.jid) : ''
 	}
 	return admins
-}
+} // Full Fix Cok Jangan Di Edit Lagi! By @arifirazzaq2001
