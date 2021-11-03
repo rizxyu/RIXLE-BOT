f3c1884d2b1aeb3491f3efd1247bd02d5fddc0ec
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-03 10:15:00 +0700

    full fix @arifirazzaq2001

--- commands/function/antilinktelegram.js
+++ commands/function/antilinktelegram.js
@@ -1,12 +1,24 @@
-/*module.exports = { 
+module.exports = { 
 name: "antilinktelegram", //By @arifirazzaq2001
-async functions(m) { 
-const gMdata = m.isGroup ? await conn.groupMetadata(from) : ''
+admin: true,
+botAdmin: true,
+
+async functions(m) {
+let { conn } = data
+const botNumber = conn.user.jid
+const gMdata = m.isGroup ? await conn.groupMetadata(m.chat) : ''
 const grupAdmin = m.isGroup ? getGroupAdmin(gMdata.participants) : ''
 const isAdmin = grupAdmin.includes(m.sender)
- if (m.message || m.text.includes("t.me")) {
+const groupMembers = m.isGroup ? gMdata.participants : ''
+const groupAdmins = m.isGroup ? getGroupAdmin(groupMembers) : ''
+const isGroupAdmins = groupAdmins.includes(m.sender) || false 
+const isBotGroupAdmins = groupAdmins.includes(botNumber)
+
+ if (m.text.includes("t.me/")) {
  if (m.sender === conn.user.jid) return
  if (m.sender === isAdmin) return
+ if (isGroupAdmins) return // Melewati Transisi Ketika Detected Suatu Grup Yang Bukan Admin
+ if (!isBotGroupAdmins) return //Detect Clone Bot Harus Admin
 m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Telegram Terdeteksi!* \n_Maaf Anda Di Keluarkan._`)
 await conn.groupRemove(m.chat, [m.sender])
 }
@@ -19,4 +31,4 @@ function getGroupAdmin(participants) {
 		i.isAdmin ? admins.push(i.jid) : ''
 	}
 	return admins
-}*/
+} // Full Fix Cok Jangan Di Edit Lagi! By @arifirazzaq2001
