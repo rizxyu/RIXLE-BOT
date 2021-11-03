014cdc3ce43fe795588bf98ac29f0255b785f402
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-03 09:41:00 +0700

    full fix @arifirazzaq2001

--- commands/function/antilinkinstagram.js
+++ commands/function/antilinkinstagram.js
@@ -1,23 +1,28 @@
 module.exports = { 
-name: "antilinkinstagram", //By @arifirazzaq2001
+name: "antilinkgroup", //By @arifirazzaq2001
 admin: true,
 botAdmin: true,
 
-async functions(m) { 
+async functions(m) {
+let { conn } = data
+const botNumber = conn.user.jid
 const gMdata = m.isGroup ? await conn.groupMetadata(m.chat) : ''
 const grupAdmin = m.isGroup ? getGroupAdmin(gMdata.participants) : ''
 const isAdmin = grupAdmin.includes(m.sender)
-const isGroupAdmins = getGroupAdmin.includes(m.sender) || false
+const groupMembers = m.isGroup ? gMdata.participants : ''
+const groupAdmins = m.isGroup ? getGroupAdmin(groupMembers) : ''
+const isGroupAdmins = groupAdmins.includes(m.sender) || false 
+const isBotGroupAdmins = groupAdmins.includes(botNumber)
 let babi = "https://instagram.com/";
 let anjeng = "https://www.instagram.com/";
 
  if (m.text.includes(babi) && m.text.includes(anjeng)) {
  if (m.sender === conn.user.jid) return
  if (m.sender === isAdmin) return
- if (m.sender === isGroupAdmins) return m.reply("Bot Bukan Admin")
+ if (isGroupAdmins) return
+ if (!isBotGroupAdmins) return
 m.reply(`Maaf @${m.sender.split("@")[0]} *Anti Link Instagram Terdeteksi!* \n_Maaf Anda Di Keluarkan._`)
 await conn.groupRemove(m.chat, [m.sender])
-}
 }
 }
 }
@@ -28,4 +33,4 @@ function getGroupAdmin(participants) {
 		i.isAdmin ? admins.push(i.jid) : ''
 	}
 	return admins
-}
+} // Full Fix Cok Jangan Di Edit Lagi! By @arifirazzaq2001
