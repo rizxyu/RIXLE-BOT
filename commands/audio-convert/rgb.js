ef4af5275a4a7ad061e06e403578a7b0e3a427a0
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-11-04 13:13:14 +0700

    gatau work pa kaga
    kapo ga work ya fix lah tolol

--- commands/audio-convert/rgb.js
+++ commands/audio-convert/rgb.js
@@ -1,4 +1,3 @@
-/* EROR CUY
 const fs = require('fs')
 const { exec } = require('child_process')
 
@@ -12,25 +11,23 @@ utilisation: userbot.prefix + "rgb reply",
 async execute(m) {
 let { conn, args } = data
 try {
-        let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
-        let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
-        if (/image/.test(mime)) {
-            let media = await conn.downloadAndSaveMediaMessage(q)
+        let mime = m.quoted.mtype
+        if (/videoMessags/.test(mime)) {
+            let media = await conn.downloadAndSaveMediaMessage(await m.quoted.download())
             let ran = getRandom('.jpg')
             exec(`ffmpeg -i ${media} -vf extractplanes=r -c:v libx264rgb -pix_fmt rgb24 ${ran}`, (err, stderr, stdout) => {
                 fs.unlinkSync(media)
                 if (err) m.reply(`_*Error!*_`)
                 let buff = fs.readFileSync(ran)
-                conn.sendFile(m.chat, buff, ran, null, m, { quoted: m, mimetype: 'image/jpeg' })
+                conn.sendFile(m.chat, buff, ran, null, m)
                 fs.unlinkSync(ran)
             })
         } else m.reply(`Balas video yang ingin diubah dengan caption *${userbot.prefix}rgb*`)
     } catch (e) {
-        throw e
+        m.reply(e)
     }
 }
 }
 const getRandom = (ext) => {
     return `${Math.floor(Math.random() * 10000)}${ext}`
 }
-*/
