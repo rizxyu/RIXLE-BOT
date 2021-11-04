0999db5b5286399d646aa5823e8ad5836efdfe2a
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-04 13:08:14 +0700

    Update cut.js

--- commands/audio-convert/cut.js
+++ commands/audio-convert/cut.js
@@ -1,1 +1,37 @@
-const _0x386cea=_0xa2fe;(function(_0x34f617,_0x55b235){const _0x29deff=_0xa2fe,_0x4a3d65=_0x34f617();while(!![]){try{const _0xbd0cbd=-parseInt(_0x29deff(0x153))/0x1+-parseInt(_0x29deff(0x136))/0x2*(parseInt(_0x29deff(0x13a))/0x3)+parseInt(_0x29deff(0x15a))/0x4*(-parseInt(_0x29deff(0x14c))/0x5)+-parseInt(_0x29deff(0x13b))/0x6*(-parseInt(_0x29deff(0x151))/0x7)+parseInt(_0x29deff(0x154))/0x8*(parseInt(_0x29deff(0x142))/0x9)+-parseInt(_0x29deff(0x140))/0xa+parseInt(_0x29deff(0x146))/0xb*(parseInt(_0x29deff(0x158))/0xc);if(_0xbd0cbd===_0x55b235)break;else _0x4a3d65['push'](_0x4a3d65['shift']());}catch(_0x2a5278){_0x4a3d65['push'](_0x4a3d65['shift']());}}}(_0xec11,0x6b497));function _0xec11(){const _0xed0aa6=['mtype','1037796BnEmzX','cut\x20audio\x20music','124ZXBFFQ','mimetype','16bIFfHH','ffmpeg\x20-ss\x20','test','audio/mp4','223989IyYEIf','1416kgjktj','chat','sendFile','random','cut','3750820fggyMr','msg','4473paqIZU','child_process','readFileSync','\x20-i\x20','165ENXMBg','Balas\x20vn/audio\x20yang\x20ingin\x20diubah\x20dengan\x20caption\x20*','\x20-c\x20copy\x20','reply','audio','unlinkSync','94440FBkwtd','quoted','prefix','exports','detik?','25592KJBBTc','downloadAndSaveMediaMessage','812279UZZKcs','10456fEmLbF','\x20-t\x20','floor'];_0xec11=function(){return _0xed0aa6;};return _0xec11();}function _0xa2fe(_0x3049c7,_0x28f7af){const _0xec1186=_0xec11();return _0xa2fe=function(_0xa2fe3a,_0x4c8c48){_0xa2fe3a=_0xa2fe3a-0x136;let _0x4fb28b=_0xec1186[_0xa2fe3a];return _0x4fb28b;},_0xa2fe(_0x3049c7,_0x28f7af);}const fs=require('fs'),{exec}=require(_0x386cea(0x143));module[_0x386cea(0x14f)]={'name':[_0x386cea(0x13f)],'type':[_0x386cea(0x14a)],'useLimit':!![],'description':_0x386cea(0x159),'utilisation':userbot[_0x386cea(0x14e)]+_0x386cea(0x13f),async 'execute'(_0x522fba){const _0x2536c2=_0x386cea;let {conn:_0x106e3f,args:_0x9f14}=data;try{if(!_0x9f14[0x0])return _0x522fba['reply']('detik?');if(!_0x9f14[0x1])return _0x522fba[_0x2536c2(0x149)](_0x2536c2(0x150));let _0x56ffda=_0x522fba['quoted']?{'message':{[_0x522fba['quoted'][_0x2536c2(0x157)]]:_0x522fba[_0x2536c2(0x14d)]}}:_0x522fba,_0x2f2e54=(_0x522fba[_0x2536c2(0x14d)]?_0x522fba[_0x2536c2(0x14d)]:_0x522fba[_0x2536c2(0x141)])[_0x2536c2(0x15b)]||'';if(/audio/[_0x2536c2(0x138)](_0x2f2e54)){let _0x3b13e3=await _0x106e3f[_0x2536c2(0x152)](_0x56ffda),_0x496d93=getRandom('.mp3');exec(_0x2536c2(0x137)+_0x9f14[0x0]+_0x2536c2(0x145)+_0x3b13e3+_0x2536c2(0x155)+_0x9f14[0x1]+_0x2536c2(0x148)+_0x496d93,(_0x590641,_0x26ff7e,_0x7fb6fd)=>{const _0x7c18ad=_0x2536c2;fs[_0x7c18ad(0x14b)](_0x3b13e3);if(_0x590641)_0x522fba['reply']('_*Error!*_');let _0x1b20d2=fs[_0x7c18ad(0x144)](_0x496d93);_0x106e3f[_0x7c18ad(0x13d)](_0x522fba[_0x7c18ad(0x13c)],_0x1b20d2,_0x496d93,null,_0x522fba,!![],{'quoted':_0x522fba,'mimetype':_0x7c18ad(0x139)}),fs[_0x7c18ad(0x14b)](_0x496d93);});}else _0x522fba[_0x2536c2(0x149)](_0x2536c2(0x147)+userbot['prefix']+'cut*');}catch(_0x4ad686){throw _0x4ad686;}}};const getRandom=_0x238f21=>{const _0x1117c6=_0x386cea;return''+Math[_0x1117c6(0x156)](Math[_0x1117c6(0x13e)]()*0x2710)+_0x238f21;};
+const fs = require('fs')
+const { exec } = require('child_process')
+
+module.exports = {
+name: ["cut"],
+type: ["audio"],
+useLimit: true,
+description: "cut audio music",
+utilisation: userbot.prefix + "cut",
+
+async execute(m) {
+let { conn, args } = data
+try {
+if (!args[0]) return m.reply('detik?')
+if (!args[1]) return m.reply('detik?')
+        let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
+        let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
+        if (/audio/.test(mime)) {
+            let media = await conn.downloadAndSaveMediaMessage(q)
+            let ran = getRandom('.mp3')
+            exec(`ffmpeg -ss ${args[0]} -i ${media} -t ${args[1]} -c copy ${ran}`, (err, stderr, stdout) => {
+                fs.unlinkSync(media)
+                if (err) m.reply(`_*Error!*_`)
+                let buff = fs.readFileSync(ran)
+                conn.sendFile(m.chat, buff, ran, null, m, true, { quoted: m, mimetype: 'audio/mp4' })
+                fs.unlinkSync(ran)
+            })
+        } else m.reply(`Balas vn/audio yang ingin diubah dengan caption *${userbot.prefix}cut*`)
+    } catch (e) {
+        throw e
+    }
+}
+}
+//Punya mamang rizky
+const getRandom = (ext) => {
+    return `${Math.floor(Math.random() * 10000)}${ext}`
+}
