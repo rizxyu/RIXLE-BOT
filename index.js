0399eec92592d354b4c1f42b8b21c23f551900bf
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-06 05:31:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ global.document = mediaType.document
 
 conn.version = [ 2, 2140, 12 ]
 conn.logger.level = "warn"
-conn.browserDescription = ['Den<R>', 'SAFARI', '8.1']
+conn.browserDescription = ['R I X L E   B O T', 'SAFARI', '8.1']
 
 if (fs.existsSync('./session.json')) conn.loadAuthInfo('./session.json')
 conn.on('qr', qr => {
