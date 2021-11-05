ad007b32c52ab259fb2883aa6baaa3c3b7b15e20
namikulaufa <85009935+namikulaufa@users.noreply.github.com>
2021-11-05 11:04:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 
 userbot = {
  Prem: JSON.parse(fs.readFileSync('./db/premium.json')),
- owner: [ "6281261324817", "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
+ owner: [ "6285217835752", "380946298137","6289646548836" ],
   MONGO_URI: "mongodb+srv://<username>:<password>@cluster0.eyx0e.mongodb.net/<dbname>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
@@ -13,16 +13,16 @@ userbot = {
    limit: 100,
    eror: "Sepertinya Eror",
    waits: "Sedang di proses",
-   butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU', // buat Image Button
-   packname: "RIXLE-BOT OFFICIAL",
-   author: '© 2021 - FearTeam',
+   butmag: 'https://i.ibb.co/XJHWhvb/380-94-629-8137-20211105-110105.jpg', // buat Image Button
+   packname: "KIRONEKO-BOT",
+   author: '© 2021 - ❤',
     setting: {
-    admin: "only admin",
-    group: "only group",
-    owner: "owner only pack",
-    jadibot: "jadibot only",
+    admin: "only admin can access this",
+    group: "only group can access this",
+    owner: "owner only pack can access this",
+    jadibot: "jadibot only can access this",
     botadmin: "bot harus menjadi admin",
-    nsfw: "FITUR BELUM DIAKTIFKAN!"
+    nsfw: "FITUR NIKI DERENG DIPUNMURUPAKEN!"
     }
 }
 module.exports = userbot
