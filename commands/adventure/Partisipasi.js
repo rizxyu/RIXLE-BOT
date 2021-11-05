0c372f3f429e6bde717e853c53e1aff5a49ff353
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-05 15:26:39 +0700

    Update Partisipasi.js

--- commands/adventure/Partisipasi.js
+++ commands/adventure/Partisipasi.js
@@ -1,1 +1,18 @@
+/*
+Masih Otak Atik
 
+const db = require('../../db/peserta.json')//au
+const event = require('../../event.json')
+
+if (db = m.sender) return m.reply ('anda belum ikut partisipasi adventure dungeon silahkan daftar terlebih dahulu')
+
+m.reply(`
+Total peserta/partisipasi Adventure,dungeon💠
+${db.length} player
+
+🎪Event 
+${event.log == '' ? 'tidak ada event yang tersedia' || event.log}
+`)
+
+
+*/
