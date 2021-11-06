ef30a83da7ddee6d52af38c580d9b3ffde47f440
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-11-06 18:42:35 +0700

    gpp

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -7,7 +7,7 @@ utilisation: userbot.prefix + "tiktok <url>",
 
 async execute(m) {
 let { args } = data
-let url = m.quoted.text || args[0]
+let url = m.quoted.text | args[0]
 if (!url) return m.reply("please input url")
 try {
 let ttdata = await tiktokmusic(url)
