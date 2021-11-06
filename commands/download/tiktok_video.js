6d49cbfd994fe668e7f8ea36e26c9e42b52df631
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2021-11-06 18:42:11 +0700

    Update tiktok_video.js

--- commands/download/tiktok_video.js
+++ commands/download/tiktok_video.js
@@ -12,10 +12,9 @@ if (!url) return m.reply("please input url")
 try {
 let ttdata = await tiktokmusic(url)
 let teks = `*Nickname :* ${ttdata.meta.author.nickname}\n*Desc :* ${ttdata.meta.desc}\n*Duration* : ${ttdata.meta.video.duration}\n\n_Pilih Type Dibawah Ini_  ${m.mention}`
-conn.send2ButtonImg(m.chat, teks, await Ft.getBuffer(ttdata.meta.video.cover), userbot.packname, "MP3","tmusic "+url, "MP4", "tnowm "+url, {contextInfo:{"mentionedJid": conn.parseMention(teks)}})
+conn.send2ButtonImg(m.chat, teks, await Ft.getBuffer(ttdata.meta.video.cover), userbot.packname, "MP3 🎧","tmusic "+url, "MP4 🎥", "tnowm "+url, {contextInfo:{"mentionedJid": conn.parseMention(teks)}})
 } catch (e) {
 m.reply(Ft.util.format(e))
 }
 }
-//Uhahahah
 }
