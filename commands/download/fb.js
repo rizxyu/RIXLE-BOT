0e3ad11db49de866cc591286afea244a7a27e38c
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-07 14:53:00 +0700

    Update fb.js

--- commands/download/fb.js
+++ commands/download/fb.js
@@ -1,3 +1,5 @@
+const toolfb = require("fb-downloads")
+
 module.exports = {
 name: ["fb"],
 type: ["download"],
@@ -10,12 +12,8 @@ let { conn, args } = data
 
 if (!args[0]) return m.reply('urlnya mana')
 if (!args[0].includes("facebook")) return m.reply('url is wrong')
-
-(async () => {
- var toolfb = require("fb-downloads");
- var videolink = await toolfb.getVideoUrl(args[0]);
+ const videolink = await toolfb.getVideoUrl(args[0])
  conn.sendFile(m.chat, videolink.sd, 'fb.mp4', `link hd: ${videolink.sd}\n\n*Jangan lupa donasi ke owner ya!*`, m)
-})();
 }
 }
 /*
