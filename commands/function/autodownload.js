84b849f5a2fcb47bd02394dc75d4199f4213a749
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-08 05:45:32 +0700

    Update autodownload.js

--- commands/function/autodownload.js
+++ commands/function/autodownload.js
@@ -2,13 +2,14 @@ const { servers, yta, ytv } = require('../../Lib/y2mate')
 const fetch = require('node-fetch')
 let yts = require('yt-search')
 const { tiktok } = require("../../Lib/scrape.js")
+const { fbdl } = require('../../Lib/fbdl')
 
 module.exports = { 
 name: "autodownlaod", 
 async functions(m) {
 let { conn } = data
 
-if (/^https?:\/\/.*youtu/i.test(m.text)) {
+   if (/^https?:\/\/.*youtu/i.test(m.text)) {
         let results = await yts(m.text)
         let vid = results.all.find(video => video.seconds < 3600)
         if (!vid) return m.reply('Video/Audio Tidak ditemukan')
@@ -31,10 +32,19 @@ if (/^https?:\/\/.*youtu/i.test(m.text)) {
         m.reply('SEDANG DIPROSES')
         await conn.sendFile(m.chat, dl_link, title + ".mp3", null, m)
    }
-     if (/^https?:\/\/.*vt/i.test(m.text)) {
+    if (/^https?:\/\/.*vt/i.test(m.text)) {
      let ttdata = await tiktok(m.text)
      conn.sendMessage(m.chat, await (await Ft.fetch(ttdata.result.nowm)).buffer(),"videoMessage",{quoted:m})
-     }
+   }   
+    if (/^https?:\/\/.facebook/i.test(m.text)) {
+     const v = await fbdl(m.text)
+     conn.sendFile(m.chat, v.hasil.link_high, 'fb.mp4', `\n*Berhasil Mendapatkan Video*\n\n⬇️Post by ${v.hasil.author}\n📖desk: ${v.hasil.title}`, m)
+   }
+    if (/^https?:\/\/.twitter/i.test(m.text)) {
+    var res = await fetch(`https://rizapi.herokuapp.com/api/twitter?url=${m.text}`)
+    let json = await res.json()
+    conn.send3Button(m.chat,`*Twitter Downloader*\n Pilih type dibawah`, userbot.prefix, `HD`, `thd ${args[0]}`, `SD`, `tsd ${args[0]}`, `AUDIO`, `taudio ${args[0]}`, m)
+   }
 }
 }
 //Madeby Rizxyu
