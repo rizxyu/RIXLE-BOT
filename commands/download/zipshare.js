027d51d9dfd5249ecae2600010bd1541b3c9ff04
Den R <90736052+Riz24@users.noreply.github.com>
2021-11-07 20:51:00 +0700

    Add files via upload

---
+++ commands/download/zipshare.js
@@ -0,0 +1,35 @@
+const fs = Ft.fs
+const Zipdl = require("../../Lib/zipdown").ZippDL
+
+module.exports = {
+name: ["zippydl"],
+type: ["download"],
+description: "download file from zippyshare with url",
+utilisation: userbot.prefix + "zippyshare <link>",
+
+
+async execute(m) {
+let { conn, text } = data
+const isUrl = (url) =>  url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&/=]*)/, 'gi'))
+try {
+if (!text) return m.reply("url ?")
+if(!isUrl(text) && !text.includes('zippyshare')) return m.reply("url salah pack")
+teks = text
+res = await Zipdl(teks)
+               result = `*Data Berhasil Didapatkan!*
+               
+        *📛 Nama* : ${res.title}
+        *⚖️ Ukuran* : ${res.size}
+        *🅾️ Type* : ${res.filetype}
+        *🗓️ Upload* : ${res.upload}
+        *🔗 Link* : ${res.url}
+
+_*File sedang dikirim, Silahkan tunggu beberapa menit*_`
+
+m.reply(Ft.util.format(result))
+conn.sendFile(m.chat, res.url, res.filetype, null, m)
+} catch (e) {
+console.log(e)
+}
+}
+}
