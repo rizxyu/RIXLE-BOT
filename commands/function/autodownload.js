2aca81aee215c8358ccb0548e6dd518b674cca09
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-07 15:50:08 +0700

    Create autodownload.js

---
+++ commands/function/autodownload.js
@@ -0,0 +1,40 @@
+const { servers, yta, ytv } = require('../../Lib/y2mate')
+const fetch = require('node-fetch')
+let yts = require('yt-search')
+
+module.exports = { 
+name: "autodownlaod", 
+async functions(m) {
+let { conn } = data
+
+if (/^https?:\/\/.*youtu/i.test(m.text)) {
+        let results = await yts(url)
+        let vid = results.all.find(video => video.seconds < 3600)
+        if (!vid) return m.reply('Video/Audio Tidak ditemukan')
+        let yt = false
+        let usedServer = servers[0]
+        for (let i in servers) {
+            let server = servers[i]
+            try {
+                yt = await yta(vid.url, server)
+                yt2 = await ytv(vid.url, server)
+                usedServer = server
+                break
+            } catch (e) {
+                m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+            }
+        }
+        if (yt === false) return m.reply(eror)
+        if (yt2 === false) return m.reply(eror)
+        let { dl_link, thumb, title, filesize, filesizeF } = yt
+        await this.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
+*Judul:* ${title}
+*Ukuran File Audio:* ${filesizeF}
+*Ukuran File Video:* ${yt2.filesizeF}
+*Server y2mate:* ${usedServer}
+
+Untuk memotong Audio ${userbot.prefix}cut detik detik
+`.trim(), userbot.botwm , 'video', `video ${vid.url}`)
+    }
+}
+}
