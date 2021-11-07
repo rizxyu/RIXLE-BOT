332b98a03d4195b250db07510b934cd107f22f9f
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-07 14:54:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 
 userbot = {
  Prem: JSON.parse(fs.readFileSync('./db/premium.json')),
- owner: [ "6281261324817", "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
+ owner: [ "6281261324817", "6281916259786", "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
   MONGO_URI: "mongodb+srv://<username>:<password>@cluster0.eyx0e.mongodb.net/<dbname>?retryWrites=true&w=majority",
    mess: {
    wait: "tunggu sedang di proses...",
