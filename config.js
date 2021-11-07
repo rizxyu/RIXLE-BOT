82b0b79dc229657c9fcde845342337c28ac52f5a
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-07 17:47:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ userbot = {
    wait: "tunggu sedang di proses...",
    error: "errorr!!"
    },
-   prefix: ["/"],
+   prefix: new RegExp('^[' + (opts['prefix'] || '‎xzXZ/!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&,.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']'),
    gexp: 50,
    limit: 100,
    eror: "Sepertinya Eror",
