6947c803a0cc0525b67b5441a9d60cd14cab7a7e
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-07 16:54:01 +0700

    Update tiktok.js

--- commands/download/tiktok.js
+++ commands/download/tiktok.js
@@ -12,7 +12,7 @@ try {
 if (!args[0]) return m.reply('mana urlnya')
 let ttdata = await tiktokmusic(args[0])
 let teks = `*Nickname :* ${ttdata.meta.author.nickname}\n*Desc :* ${ttdata.meta.desc}\n*Duration* : ${ttdata.meta.video.duration}\n\n_Pilih Type Dibawah Ini_  ${m.mention}`
-conn.send2ButtonImg(m.chat, teks, await Ft.getBuffer(ttdata.meta.video.cover), userbot.packname, "MP3 🎧","tmusic "+url, "MP4 🎥", "tnowm "+url, {contextInfo:{"mentionedJid": conn.parseMention(teks)}})
+conn.send2ButtonImg(m.chat, teks, await Ft.getBuffer(ttdata.meta.video.cover), userbot.packname, "MP3 🎧","tmusic "+args[0], "MP4 🎥", "tnowm "+args[0], {contextInfo:{"mentionedJid": conn.parseMention(teks)}})
 } catch (e) {
 m.reply(Ft.util.format(e))
 }
