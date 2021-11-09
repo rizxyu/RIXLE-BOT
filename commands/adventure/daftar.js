ab324ab184240bfdfbfc6ac11f11d180a5b90292
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-09 19:20:18 +0700

    work @arifirazzaq2001

--- commands/adventure/daftar.js
+++ commands/adventure/daftar.js
@@ -1,28 +1,28 @@
-/*
 const fs = require ('fs') 
 const crypto = require('crypto')
 const request = require('request')
 const fetch = require('node-fetch')
-const _player = fs.writeFileSync('./tmp/adventureDB/player.json')
 
 module.exports = { // By @arifirazzaq2001
 name: ["daftar"],
 type: ["Petualangan"], 
+description: "Registry Your Account To Playing On games",
+utilisation: userbot.prefix+"daftar",
 
 async execute(m) {
 let { conn, args } = data
         const addPlayerUser = (userid, sender, serials) => {
             const obj = { id: userid, name: m.sender, serial: serials }
-            _player.push(obj)
-            fs.writeFileSync('./tmp/adventureDB/player.json', JSON.stringify(_player))
+            m._player.push(obj)
+            fs.writeFileSync('./tmp/adventureDB/player.json', JSON.stringify(m._player))
         }
         const kodePinPlayer = (size) => {
             return crypto.randomBytes(size).toString('hex').slice(0, size)
         }
         const buatCekPlayernya = (sender) => {
             let status = false
-            Object.keys(_player).forEach((i) => {
-                if (_player[i].id === m.sender) {
+            Object.keys(m._player).forEach((i) => {
+                if (m._player[i].id === m.sender) {
                     status = true
                 }
             })
@@ -31,17 +31,16 @@ let { conn, args } = data
 const gMdata = m.isGroup ? await conn.groupMetadata(m.chat) : ''
 const grupAdmin = m.isGroup ? getGroupAdmin(gMdata.participants) : ''
 const isAdmin = grupAdmin.includes(m.sender)
-const isPlayer = _player.includes(m.sender)
 const iscekplayer = buatCekPlayernya(m.sender)
 let setPin = kodePinPlayer(6)
 
+if (m._player.includes(m.sender)) {
+if (iscekplayer) return m.reply('Anda Sudah Terdaftar Sebelumnya.') 
 if (m.sender === conn.user.jid) return
 if (m.sender === isAdmin) return
-if (iscekplayer) return m.reply('Anda Sudah Terdaftar Sebelumnya.') 
-let stickGame = m.sender
 
-_player.push(m.sender)
-fs.writeFileSync('./tmp/adventureDB/player.json', JSON.stringify(_player))
+m._player.push(m.sender)
+fs.writeFileSync('./tmp/adventureDB/player.json', JSON.stringify(m._player))
 fs.writeFileSync('./tmp/adventureDB/pin.json', JSON.stringify(setPin))
 addPlayerUser(m.sender, kodePinPlayer)
 let teks = ` _made in @arifirazzaq2001_
@@ -52,7 +51,7 @@ let teks = ` _made in @arifirazzaq2001_
 *Nama :* ${m.pushname}
 *APi :* wa.me/${m.sender.split('@')[0]}
 *Kode Pin :* ${setPin}
-*total Player :* ${_player.length}
+*total Player :* ${m._player.length}
 
 *Anda Sudah Resmi Menjadi Pengguna Gamers Dalam RixleBot*
 
@@ -61,17 +60,11 @@ let teks = ` _made in @arifirazzaq2001_
 Jadi Jangan Sampai Hilang Saya Sarankan Anda Menyalinnya Kedalam Catatan Anda/Kasih Tanda Bintang 🌟 Pesan Ini, Atau Ketik ${userbot.prefix}cekpin*
 「 RIXLE-BOT 」*
 `
-conn.sendMessage(m.chat, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc675b80s5tCNwEdtfTpmfebv1uUlk4f2huA&usqp=CAU', MessageType.image, 
-{
-caption: teks, 
- quoted: m, 
-  contextInfo: { 
-      mentionedJid: [m.sender] 
-      }
-    })
+m.reply(teks)
 console.log(`${m.pushname} Berhasil Membuat Json Player Games`)
 } 
 } 
+}
 
 function getGroupAdmin(participants) {
 	admins = []
@@ -80,4 +73,3 @@ function getGroupAdmin(participants) {
 	}
 	return admins
 }
-*/
