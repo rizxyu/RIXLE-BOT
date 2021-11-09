90d85c3cd2a21be9304ee792a61c16226b59c3c1
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-09 16:17:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,74 @@
 "use strict";
 const fs = require("fs")
-//const qrcode = require("qrcode")
+const qrcode = require("qrcode")
 const Baileys = "@adiwajshing/baileys";
 const { WAConnection: _WAConnection } = require("@adiwajshing/baileys");
 const WAConnection = require('./Lib/simple').WAConnection(_WAConnection);
 const { Functions } = require('./Lib/Functions');
-const { JsonDB } =  require("node-json-db")
+const { start } = require('./Lib/banner');
+const { JsonDB } = require("node-json-db")
 const { Config } = require('node-json-db/dist/lib/JsonDBConfig')
-  
 
-global.antidelete = false
-  global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
-    global.Ft = new Functions();// Menghubungkan dari Function.js
+global['write'] = {};
+ global['write']['words'] = JSON.parse(fs.readFileSync('./tmp/debug.json')); // Biar Bingung Orang Awam, dan Susah Recode :v
+  global.antidelete = false
+   global.welcome = true
+    global.API = (name, path = '/', query = {}, apikeyqueryname) => (name in global.APIs ? global.APIs[name] : name) + path + (query || apikeyqueryname ? '?' + new URLSearchParams(Object.entries({ ...query, ...(apikeyqueryname ? { [apikeyqueryname]: global.APIKeys[name in global.APIs ? global.APIs[name] : name] } : {}) })) : '')
+     global.Ft = new Functions(); // Menghubungkan dari Function.js
       global.mediaType = require(Baileys).MessageType //Biar keren hehe
-        global.conn = new WAConnection(); //Wa Connect dari baileys
-          global.botuser = require('./config')//Menghubungkan Ke Connection string
-            global.Events = {}
-             global.baileys = Baileys //Hehe
-               global.db = new JsonDB(new Config("database", true, false, '/'));
-                 global.Public = false
-                   global.scrap = require("./Lib/scrape");
+       global.conn = new WAConnection(); //Wa Connect dari baileys
+        global.botuser = require('./config') //Menghubungkan Ke Connection string
+         global.Events = {}
+          global.baileys = Baileys //Hehe
+           global.db = new JsonDB(new Config("database", true, false, '/'));
+            global.Public = false
+             global.scrap = require("./Lib/scrape");
 
-conn.version = [2, 2140, 2]
+//msgTyp
+global.text = mediaType.text
+global.image = mediaType.image
+global.video = mediaType.video
+global.audio = mediaType.audio
+global.location = mediaType.location
+global.document = mediaType.document
+
+conn.version = [ 2, 2140, 12 ]
 conn.logger.level = "warn"
-conn.browserDescription = ['R I X L E', 'EDGE', '94.0.992.50']
+conn.browserDescription = ['Rixle Type 3', 'SAFARI', '8.1']
 
-if (fs.existsSync('./session.json')) conn.loadAuthInfo('./session.json')
-   conn.on('qr', qr => {
-   console.log(`PLEASE SCAN QR`)
+// Mengurangi logger mempercepat Balas Pesan Fixed @arifirazzaq2001
+if (fs.existsSync(global.write.words.qrcode)) conn.loadAuthInfo(global.write.words.qrcode)
+conn.on('qr', qr => {
+ conn.logger.warn("[!] Scan Kode QR Diatas, Expired dalam 30 detik")
+  })
+   conn.on('credentials-updated', () => {})
+    conn.on('connecting', () => {})
+     conn.on("open", () => {
+      conn.on("ws-close", async() => {})
+       conn.on("close", async() => {}) 
+
+const authInfo = conn.base64EncodedAuthInfo() 
+fs.writeFileSync(global.write.words.qrcode, JSON.stringify(authInfo, null, '\t'))
+
+console.clear()
+conn.logger.warn('Berhasil Perbarui Jaringan\n')
+start('\n',
+    conn.logger.warn('\n')
+  )
 })
 
-conn.on('connecting', () => {
-   console.log(`Connecting...`)
-})
-conn.on("open", () => {
-const authInfo = conn.base64EncodedAuthInfo()
-let stats = {
-status: 200,
-message: "success",
-info: "berhasil masuk ke dalam baileys"
-}
-conn.on("ws-close", async() => {
-conn.logger.warn('Connected Timeout')
-})
-conn.on("close", async() => {
-conn.logger.warn('Closed Connection')
-})
-console.log(stats)
-fs.writeFileSync('./session.json', JSON.stringify(authInfo, null, '\t'))
-})
-/*
 setInterval(() => {
- conn.setStatus(`Ｓｔａｔｕｓ Ｂｏｔ:
-› Runtime: ${Ft.count(process.uptime())}
-› Hostname: ${Ft.os.hostname()}
-| DILARANG KERAS MENGKLAIM SC BOT/BOT | BY RIZXYU`).catch((_) => _)
+ conn.setStatus(`${Ft.count(process.uptime())} | ${Ft.os.hostname()} Fear Team | Your Bio ☣`).catch((_) => _)
 },1000)
-*/
- require('./src/loader')
+ require('./src/loader');
+
  async function run() {// Function biar bisa run bot
  let message = require('./action/chats');
  let action = require('./action/action');
  await conn.connect();
  conn.message = message.msg
  conn.on('chat-update', conn.message);
- //conn.on('group-participants-update', action.groupUpdate);
+ conn.on('group-participants-update', action.groupUpdate); // ivan tolol
  }
  Ft.action()
- console.clear();
  run();// Menjalankan Bot
