993ba332b3b37d8de4c8571cdc858b020059cb34
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-09 16:45:15 +0700

    Update antitoxic.js

--- commands/function/antitoxic.js
+++ commands/function/antitoxic.js
@@ -3,10 +3,6 @@ module.exports = {
 name: "antitoxic",  
 async functions(m) { 
 let { conn, text } = data
-let badwordRegex = /anjing,kontol,anj,asu,asw,pantek,memek/
-
-let name = conn.getName(m.sender)
-
 if (/^(kntl(bacot(anj?ing?Ng)asu)kanjut)/i.test(m.text)) {
 m.reply('Terdeteksi Menggunakan bahasa Toxic')
 }
