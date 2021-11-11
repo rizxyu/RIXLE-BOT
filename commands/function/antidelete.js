82de040ee07e3b48e21e7658958b4ef96d7c544a
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-11 10:59:46 +0700

    Update antidelete.js

--- commands/function/antidelete.js
+++ commands/function/antidelete.js
@@ -6,17 +6,18 @@ botAdmin: true,
 async functions(m) {
 let { conn } = data
 const botNumber = conn.user.jid
-const gMdata = m.isGroup ? await conn.groupMetadata(m.chat) : '' //Fixed By @arifirazzaq2001
+const gMdata = m.isGroup ? await conn.groupMetadata(m.chat) : '' 
 const grupAdmin = m.isGroup ? getGroupAdmin(gMdata.participants) : ''
 const isAdmin = grupAdmin.includes(m.sender)
 const groupMembers = m.isGroup ? gMdata.participants : ''
 const groupAdmins = m.isGroup ? getGroupAdmin(groupMembers) : ''
-const isGroupAdmins = groupAdmins.includes(m.sender) || false // sender Yang Membuat Bingung 😩 Sudah di fix
-const isBotGroupAdmins = groupAdmins.includes(botNumber) || false // Tidak Spam Text Saat Bot Bukan Admin | Fixed By @arifirazzaq2001
+const isGroupAdmins = groupAdmins.includes(m.sender) || false 
+const isBotGroupAdmins = groupAdmins.includes(botNumber) || false 
 
+if (global.antidelete) {
 await conn.reply(m.key.remoteJid, `terdeteksi hapus pesan`, m)
-
 conn.copyNForward(m.key.remoteJid, m.message).catch(e => console.log(e, m))
+  }
 }
 }
 
