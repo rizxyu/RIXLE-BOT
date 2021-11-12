6a5b90723f3c30b113cb61d69050db5421309014
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-12 07:42:14 +0700

    Create afk.js

---
+++ commands/fun/afk.js
@@ -0,0 +1,19 @@
+let fs = require('fs')
+
+module.exports = {
+name: ["afk"],
+type: ["fun"],
+description: "afk",
+utilisation: userbot.prefix + "afk <alasan>",
+
+async execute(m) {
+let { conn, text } = data
+
+const json = JSON.parse(fs.readFileSync('./db/afk.json'))
+
+json.push(m.sender, text,+ new Date)
+fs.writeFileSync('./db/afk.json', JSON.stringify(json))
+m.reply(`${conn.getName(m.sender)} sekarang afk dengan alasan ${text ? 'tidak ada alasan' : '' || text}`)
+}
+}
+//:V
