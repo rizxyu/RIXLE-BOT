c3f79ea1914221c35b14785dfefbef8c518aa871
Rizky <88314302+Rizxyu@users.noreply.github.com>
2021-11-14 18:46:39 +0700

    Update cmd.js

--- action/cmd.js
+++ action/cmd.js
@@ -1,6 +1,9 @@
+let fs = require('fs')
+
 module.exports = {
 async Command(conn, m) {
 try {
+Let dftr = fs.readFileSync('./db/daftar.json')
 let usedPrefix
 if (typeof m.text !== 'string') m.text = ''
 let groupMetadata = m.isGroup ? await conn.groupMetadata(m.chat) : {} || {}
@@ -10,6 +13,8 @@ let bot = m.isGroup ? participants.find(u => u.jid == conn.user.jid) : {}
 let isBotAdmin = bot.isAdmin || bot.isSuperAdmin || false
 let isAdmin = user.isAdmin || user.isSuperAdmin || false // Is User Admin?
 let isOwner = userbot['owner'].map(v => v + '@s.whatsapp.net').includes(m.sender) || false
+let isDaftar = dftr.includes(m.sender) || false //gabut ajah
+
 global.dfail = (type, m, conn) => {
 let msgnye = {
 grup: global.userbot['setting'].group,
@@ -17,6 +22,7 @@ admin: global.userbot['setting'].admin,
 botAdmin: global.userbot['setting'].botadmin,
 user: global.userbot['setting'].jadibot,
 owner: global.userbot['setting'].owner,
+daftar: global.userbot['setting'].daftar,
 }[type]
 if (msgnye) return m.reply(msgnye)
 }
@@ -74,7 +80,8 @@ let custom = cmd.custom
 if (!custom) continue
 if (m.text.startsWith(cmd.name)) {
 if (cmd.admin && !isAdmin) return dfail("admin", m, conn)
-if (cmd.owner && !isOwner) return 
+if (cmd.owner && !isOwner) return dfail("owner", m, conn)
+if (cmd.daftar && !isDaftar) return dfail("daftar", m, conn)
 if (cmd.botAdmin && !isBotAdmin) return dfail("botAdmin", m, conn)//Si paujan ketinggalan jadi gw tambahin
 console.log(cmd.name)
 await cmd.execute.call(conn, m, data)
@@ -88,7 +95,8 @@ cmd = Array.isArray(Command.name) ? Command.name.some(cmd => cmd === global.comm
 if (!global.command) continue
 if (!cmd) continue
 if (Command.admin && !isAdmin) return dfail("admin", m, conn)
-if (Command.owner && !isOwner) return
+if (Command.owner && !isOwner) return dfail("owner", m, conn)
+if (Command.daftar && !isDaftar) return dfail("daftar", m, conn)
 if (Command.botAdmin && !isBotAdmin) return dfail("botAdmin", m, conn)
 global.db.data[global.command] += 1
 await Command.execute.call(conn, m, data)
