2926fad887bce5c70c9aa9203aa1d92b428bb5a1
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-11-14 16:50:42 +0700

    Update ytmp3.js

--- commands/download/ytmp3.js
+++ commands/download/ytmp3.js
@@ -3,7 +3,7 @@ let limit = 30
 const { servers, yta } = require('../../Lib/y2mate')
 const fetch = require('node-fetch')
 module.exports = {
-name: ["ytmp3"],
+name: ["ytmp3", "yta"],
 type: ["download"],
 useLimit: true,
 description: "download audio from youtube url",
@@ -18,8 +18,7 @@ let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (limit) * 1024 < filesize  
   m.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : 'compressing...')
-  conn.sendButtonLoc(m.chat, await (await fetch (thumb)).buffer(), `
-  *ＹＴＭＰ3 ＹＯＵＴＵＢＥ*
+  conn.sendButtonLoc(m.chat, await (await fetch (thumb)).buffer(), `*ＹＴＭＰ3 ＹＯＵＴＵＢＥ*
   *Title:* ${title}
   *Size:* ${filesizeF}`, userbot.packname, 'Menu', 'menu', m)
   if (!isLimit) conn.sendFile(m.chat, dl_link, title + ".mp3", null, m)
