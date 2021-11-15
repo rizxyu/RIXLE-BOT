c95692b1201da991a3f77eab3e684cb77b42d45e
Rizky <88314302+Rizxyu@users.noreply.github.com>
2021-11-15 08:37:59 +0700

    Update waifu.js

--- commands/random/waifu.js
+++ commands/random/waifu.js
@@ -4,10 +4,10 @@ module.exports = {
 name: ["waifu"],
 type: ["random"],
 description: "random image waifu",
-utilisation: userbot.prefix + "cecan",
+utilisation: userbot.prefix + "waifu",
 
 async execute(m){
 let { conn } = data
- scrap.pinterest("waifu").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Ih like anime you pasti wibu",m))
+ scrap.pinterest("waifu anime").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b, null, null,m, 0, { thumbnail: Buffer.alloc(0) }))
    }
 }
