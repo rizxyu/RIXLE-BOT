c3b5a6d9e9c66c6d8e2ce44ecac30dcb01ee91b7
Rizky <88314302+Rizxyu@users.noreply.github.com>
2021-11-15 13:01:48 +0700

    Create cita2.js

---
+++ commands/random/cita2.js
@@ -0,0 +1,19 @@
+let fetch = require("node-fetch")
+let arr = []
+fetch('https://raw.githubusercontent.com/AlvioAdjiJanuar/citacita/main/citacita.txt')
+  .then(res => res.text())
+  .then(txt => arr = txt.split('\n'))
+
+module.exports = {
+name: ["cita cita", "Brando"],
+custom: true,
+owner:true,
+description: "Brando geming Bocil kematian",
+utilisation: userbot.prefix + "cita cita",
+type: ["random"],
+async execute(m){
+let cita = arr[Math.floor(Math.random() * arr.length)]
+  if (!cita) throw false
+  await conn.sendFile(m.chat, cita, null, null, m, 1, { mimetype: 'audio/mp4' })
+}
+}
