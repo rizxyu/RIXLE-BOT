58bac7a217fdb97324301605fa9db50920fb3548
Rizky <88314302+Rizxyu@users.noreply.github.com>
2021-11-15 08:51:38 +0700

    Create claim.js

---
+++ commands/adventure/claim.js
@@ -0,0 +1,30 @@
+let fs = require('fs')
+const { createHash } = require('crypto')
+let moment = require('moment-timezone')
+
+module.exports = {
+name: ["claim"],
+type: ["adventure"],
+daftar: true,
+description: "claim",
+utilisation: userbot.prefix + "claim",
+
+async execute(m) {
+let { conn, text } = data
+const json = JSON.parse(fs.readFileSync('./db/rpgdb.json'))
+
+let exp = 100
+let limit = 10
+let obat = 1
+let apel = 1
+json.push(m.sender, exp, obat, role, + new date)
+fs.writeFileSync('./db/rpgdb.json', JSON.stringify(json))
+   
+
+let capt = `
+Selamat kamu mendapatkan 100 xp > 10 limit > 1 💊 Obat > 1 🍎 Apel
+`
+
+m.reply(capt)
+}
+}
