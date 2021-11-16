f4f0db94ce661aecdfe7b5cee5d4cecc125bbc78
Rizky <88314302+Rizxyu@users.noreply.github.com>
2021-11-16 18:01:58 +0700

    Update addvn.js

--- commands/build/addvn.js
+++ commands/build/addvn.js
@@ -9,15 +9,18 @@ description: "addvn cuy",
 utilisation: userbot.prefix + "addvn nama + reply",
 
 async execute(m) {
-let { conn, args } = data
+let { conn, args, text } = data
 try {
+     if (!text) return m.reply('teks nya?')
      let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
      let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
     if (/audio/.test(mime)) {
      let media = await conn.downloadAndSaveMediaMessage(q)
-     let ran = getRandom('.mp3')
+     let ran = ( text + '.mp3')
      fs.unlinkSync(media)
      let buff = fs.readFileSync(ran)
+     m.reply(`Berhasil Menambahkan Audio Ke dalam Database untuk mengambil silahkan ketik ${userbot.prefix}getvn text`)
+     fs.unlinkSync(ran)
     } else m.reply(`Balas vn/audio yang ingin disimpan ke database dengan caption *${userbot.prefix}addvn kata*`)
 } catch (e) {
 console.log("EROR GOBLOK")
