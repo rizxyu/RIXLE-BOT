0b7dd58ea842aa8a105db9436905a5cdbaba80d0
Arifi Razzaq <77736529+Arifirazzaq2001@users.noreply.github.com>
2021-11-19 14:21:47 +0700

    Create extra.js

---
+++ commands/core/extra.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch')
+module.exports = {
+name: ["donasi"],
+type: ["default"],
+description: "no Comment",
+utilisation: userbot.prefix + "extra",
+
+async execute(m) {
+let { conn } = data
+
+} 
+}
