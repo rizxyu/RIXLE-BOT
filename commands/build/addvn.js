794a0baa1d11c500ef290fb458fffbd5ab65d7b8
Rizky <88314302+Rizxyu@users.noreply.github.com>
2021-11-23 10:20:17 +0700

    Fix

--- commands/build/addvn.js
+++ commands/build/addvn.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const { exec } = require('child_process')
 
 module.exports = {
-name: ["addvn"],
+name: ["vn"],
 type: ["media"],
 useLimit: true,
 description: "addvn cuy",
@@ -10,22 +10,71 @@ utilisation: userbot.prefix + "addvn nama + reply",
 
 async execute(m) {
 let { conn, args, text } = data
-try {
-     if (!text) return m.reply('teks nya?')
-     let q = m.quoted ? { message: { [m.quoted.mtype]: m.quoted } } : m
-     let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
-    if (/audio/.test(mime)) {
-     let media = await conn.downloadAndSaveMediaMessage(q)
-     let ran = ( text + '.mp3')
-     fs.unlinkSync(media)
-     let buff = fs.readFileSync(ran)
-     m.reply(`Berhasil Menambahkan Audio Ke dalam Database untuk mengambil silahkan ketik ${userbot.prefix}getvn text`)
-     fs.unlinkSync(ran)
-    } else m.reply(`Balas vn/audio yang ingin disimpan ke database dengan caption *${userbot.prefix}addvn kata*`)
-} catch (e) {
-console.log("EROR GOBLOK")
- }
+
+if (!args[0]) return m.reply(`
+List command Vn:
+- add
+- del
+- list
+
+Penggunaan
+${userbot.prefix + command} add (text|reply)
+`)
+
+switch (args[0]) {
+
+   case "add":
+   if(!m.quoted) return m.reply("Tag audionya!")
+   if(m.quoted.mtype && m.quoted.mtype != "audioMessage") return m.reply("Target bukan audio!")
+   if(!args[1]) return m.reply("Masukan nama file!")
+
+   if(m.quoted && m.quoted.mtype && m.quoted.mtype == "audioMessage" && m.quoted.download) {
+    var audio = await m.quoted.download()
+    fs.createWriteStream("./audio/" + args[1] + ".mp3")
+    fs.writeFileSync("./audio/" + args[1] + ".mp3", audio)
+    if(fs.existsSync("./audio/" + args[1] + ".mp3")) {
+      m.reply("Berhasil!")
+    } else {
+      m.reply("Gagal!")
+     } 
+    } else {
+     m.reply("eror")
+    }
+    break
+
+   case "del": 
+  if(fs.readdirSync("./audio/").length == 0) return m.reply("Tidak ada audio!")
+  if(!args[1] || args[1].length == 0) {
+    var str = "Tersedia :\n\n"
+    str += fs.readdirSync("./audio/").join("\n").split(".mp3").join("")
+    m.reply(str)
+  } else {
+    if(!fs.existsSync("./audio/" + args[1] + ".mp3")) return m.reply("File tidak ditemukan!")
+    fs.unlinkSync("./audio/" + args[1] + ".mp3")
+    m.reply("Berhasil!")
+  }
+
+    break
+
+   case "get":
+  if(fs.readdirSync("./audio/").length == 0) return m.reply("Tidak ada audio!")
+  if(!args[1] || args[1].length == 0) {
+    var str = "Tersedia :\n\n"
+    str += fs.readdirSync("./audio/").join("\n").split(".mp3").join("")
+    m.reply(str)
+  } else {
+    if(!fs.existsSync("./audio/" + args[1] + ".mp3")) return m.reply("File tidak ditemukan!")
+    conn.sendFile(m.chat, "./audio/" + args[1] + ".mp3", null, null, m)
+  }
+   break
+
+   case "list":
+  if(fs.readdirSync("./audio/").length == 0) return m.reply("Tidak ada audio!")
+  var str = "List audio :\n\n"
+  str += fs.readdirSync("./audio/").join("\n").split(".mp3").join("")
+  m.reply(str)
+   break
+  } 
 }
 }
-//Mengmastah🤓 anjay alok
         
