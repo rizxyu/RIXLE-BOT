2ee021ac985c02ead9915dc61e9277d555aaf28d
Rizky <88314302+Rizxyu@users.noreply.github.com>
2021-12-01 15:59:41 +0700

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,4 +1,17 @@
 const { readdirSync } = require('fs');
+const CFonts = require('cfonts')
+CFonts.say('RIXLE BOT', {
+  colors: ['#8735f2'],
+  font: 'block',
+  align: 'center',
+})
+CFonts.say(`'RIXLE BOT' MADE BY @Fear_Team`, {
+  colors: ['#8735f2'],
+  font: 'console',
+  align: 'center',
+})
+console.log(Ft.color("[ ❕ ] LOADING RUN SCRIPT...", 'green'))
+console.log(Ft.color("🗒️ Script Eror? Chat Saya om wa.me/6282328303332", 'cyan'))
 
 const uncache = (module) => {
 return new Promise((resolve, reject) => {
@@ -26,10 +39,8 @@ for (let file of commands) {
 const command = require(`../commands/${dirs}/${file}`);
 if ("functions" in command) {
 global.Functions[command.name] = command
-console.log(Ft.color(`-> Loaded Functions`, 'cyan') + Ft.color(` ${command.name}`, 'red'));
 } else {
  global.Events[command.name] = command
-console.log(Ft.color(`-> Loaded command`, 'orange') + Ft.color(` ${command.name}`, 'yellow'));
 }
 require(`../commands/${dirs}/${file}`)
 nocache(`../commands/${dirs}/${file}`, module => { 
@@ -53,5 +64,4 @@ delete global.Events[command.name]
 });
 };
 });
-
 
